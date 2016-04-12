feb42d11f97ba29e7b9aae49f2d5db1a471bfc34
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2016-04-12 17:17:10 +0000

    Update TypeBuilderExtensions
    Update TypeBuilderExtensions

--- src/System.Linq.Dynamic.Core/System/Reflection/Emit/TypeBuilderExtensions.cs
+++ src/System.Linq.Dynamic.Core/System/Reflection/Emit/TypeBuilderExtensions.cs
@@ -1,4 +1,8 @@
-﻿namespace System.Reflection.Emit
+﻿#if DNXCORE50
+using System.Linq;
+#endif
+
+namespace System.Reflection.Emit
 {
     public static class TypeBuilderExtensions
     {
@@ -6,6 +10,40 @@
         public static Type CreateType(this TypeBuilder tb)
         {
             return tb.CreateTypeInfo().AsType();
+        }
+#endif
+
+#if NET35
+        public static PropertyBuilder DefineProperty(this TypeBuilder tb, string name, PropertyAttributes attributes, CallingConventions callingConvention, Type returnType, Type[] parameterTypes)
+        {
+            return tb.DefineProperty(name, attributes, returnType, parameterTypes);
+        }
+#endif
+
+#if DNXCORE50
+        public static ConstructorBuilder DefineConstructor(this TypeBuilder tb, MethodAttributes attributes, CallingConventions callingConventions, GenericTypeParameterBuilder[] parameterTypes)
+        {
+            return tb.DefineConstructor(attributes, callingConventions, parameterTypes.Select(g => g.AsType()).ToArray());
+        }
+
+        public static FieldBuilder DefineField(this TypeBuilder tb, string fieldName, GenericTypeParameterBuilder genericTypeParameterBuilder, FieldAttributes attributes)
+        {
+            return tb.DefineField(fieldName, genericTypeParameterBuilder.AsType(), attributes);
+        }
+
+        public static MethodBuilder DefineMethod(this TypeBuilder tb, string name, MethodAttributes attributes, CallingConventions callingConvention, GenericTypeParameterBuilder returnType, Type[] parameterTypes)
+        {
+            return tb.DefineMethod(name, attributes, callingConvention, returnType.AsType(), parameterTypes);
+        }
+
+        public static MethodBuilder DefineMethod(this TypeBuilder tb, string name, MethodAttributes attributes, CallingConventions callingConvention, Type returnType, GenericTypeParameterBuilder[] parameterTypes)
+        {
+            return tb.DefineMethod(name, attributes, callingConvention, returnType, parameterTypes.Select(g => g.AsType()).ToArray());
+        }
+
+        public static PropertyBuilder DefineProperty(this TypeBuilder tb, string name, PropertyAttributes attributes, CallingConventions callingConvention, GenericTypeParameterBuilder returnType, Type[] parameterTypes)
+        {
+            return tb.DefineProperty(name, attributes, callingConvention, returnType.AsType(), parameterTypes);
         }
 #endif
     }
