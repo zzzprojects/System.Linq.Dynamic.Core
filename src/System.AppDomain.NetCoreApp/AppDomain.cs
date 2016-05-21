990110e41038ccae17ab708c1ee4840160cadb2d
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2016-05-21 12:40:11 +0200

    System.AppDomain.NetCoreApp

---
+++ src/System.AppDomain.NetCoreApp/AppDomain.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using Microsoft.DotNet.InternalAbstractions;
+using Microsoft.Extensions.DependencyModel;
+
+namespace System.AppDomain.NetCoreApp
+{
+    public sealed class AppDomain
+    {
+        public static AppDomain CurrentDomain { get; private set; }
+
+        static AppDomain()
+        {
+            CurrentDomain = new AppDomain();
+        }
+
+        /// <summary>
+        /// Gets the assemblies that have been loaded into the execution context of application domain from the supplied type.
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns>An array of assemblies in the application domain from the supplied type.</returns>
+        public Assembly[] GetAssemblies(Type type)
+        {
+            List<Assembly> assemblies = new List<Assembly>();
+
+            var dependencyModel = DependencyContext.Load(type.GetTypeInfo().Assembly);
+
+            foreach (var assemblyName in dependencyModel.GetRuntimeAssemblyNames(RuntimeEnvironment.GetRuntimeIdentifier()))
+            {
+                try
+                {
+                    try
+                    {
+                        var assembly = Assembly.Load(assemblyName);
+
+                        // just load all types and skip this assembly if one or more types cannot be resolved
+                        assembly.DefinedTypes.ToArray();
+                        assemblies.Add(assembly);
+                    }
+                    catch (Exception ex)
+                    {
+                        Debug.Write(ex.Message);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Debug.Write(ex.Message);
+                }
+            }
+
+            return assemblies.ToArray();
+        }
+    }
+}
--- test/System.Linq.Dynamic.Core.Tests/NetStandardCustomTypeProvider.cs
+++ test/System.Linq.Dynamic.Core.Tests/NetStandardCustomTypeProvider.cs
@@ -1,13 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Linq.Dynamic.Core.CustomTypeProviders;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.DotNet.InternalAbstractions;
-using Microsoft.Extensions.DependencyModel;
 using Xunit.Runner.DotNet;
 
 namespace System.Linq.Dynamic.Core.Tests
@@ -16,44 +8,13 @@ namespace System.Linq.Dynamic.Core.Tests
     {
         public HashSet<Type> GetCustomTypes()
         {
-            var assemblies = GetAssemblyList().Where(x => !x.IsDynamic).ToArray();
+            var thisType = typeof (Program);
+            var assemblies = System.AppDomain.NetCoreApp.AppDomain.CurrentDomain.GetAssemblies(thisType).Where(x => !x.IsDynamic).ToArray();
 
             var definedTypes = assemblies.SelectMany(x => x.DefinedTypes);
             var types = definedTypes.Where(x => x.CustomAttributes.Any(y => y.AttributeType == typeof(DynamicLinqTypeAttribute))).Select(x => x.AsType());
 
             return new HashSet<Type>(types);
-        }
-
-        private static List<Assembly> GetAssemblyList()
-        {
-            List<Assembly> assemblies = new List<Assembly>();
-
-
-            var dependencyModel = DependencyContext.Load(typeof(Program).GetTypeInfo().Assembly);
-
-            foreach (var assemblyName in dependencyModel.GetRuntimeAssemblyNames(RuntimeEnvironment.GetRuntimeIdentifier()))
-            {
-                try
-                {
-                    try
-                    {
-                        var assembly = Assembly.Load(assemblyName);
-
-                        // just load all types and skip this assembly of one or more types cannot be resolved
-                        var dummy = assembly.DefinedTypes.ToArray();
-                        assemblies.Add(assembly);
-                    }
-                    catch (Exception ex)
-                    {
-                        Debug.Write(ex.Message);
-                    }
-                }
-                catch
-                {
-                }
-            }
-
-            return assemblies;
         }
     }
 }
