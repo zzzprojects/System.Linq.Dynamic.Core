d97799e689051c57500fa703bd7518ddcf2bacdb
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2016-06-08 15:21:55 +0200

    Remove file

--- src/EntityFramework.DynamicLinq/IAsyncQueryProvider.cs
+++
@@ -1,8 +0,0 @@
-﻿//using System.Data.Entity.Infrastructure;
-
-//namespace EntityFramework.DynamicLinq
-//{
-//    internal interface IAsyncQueryProvider : IDbAsyncQueryProvider
-//    {
-//    }
-//}
