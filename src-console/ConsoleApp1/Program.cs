9e7c20e4de3333172b7eaaa958e157fafc60b845
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2016-06-14 08:19:48 +0200

    lambdaExpression example

--- src-console/ConsoleApp1/Program.cs
+++ src-console/ConsoleApp1/Program.cs
@@ -1,14 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Linq.Dynamic.Core;
+using System.Linq.Expressions;
 
 namespace ConsoleApp1
 {
+    class Employee
+    {
+        public string Name { get; set; }
+    }
+
     public class Program
     {
         public static void Main(string[] args)
         {
+            var emps = new Employee[] { new Employee { Name = "a" }, new Employee { Name = "b" } }.AsQueryable();
+
+            var lambdaExpression = DynamicExpressionParser.ParseLambda(false, typeof(Employee), null, "Name");
+
+            // normal
+            Expression<Func<Employee, string>> exp = u => u.Name;
+            var equery1 = emps.Select(exp);
+            Console.WriteLine("equery1 :" + string.Join(",", equery1));
+
+            // lambdaExpression
+            var body = lambdaExpression.Body;
+            var p = lambdaExpression.Parameters;
+            var x = Expression.Lambda<Func<Employee, string>>(body, p);
+
+            var equery2 = emps.Select(x);
+            Console.WriteLine("equery2 :" + string.Join(",", equery2));
         }
     }
 }
