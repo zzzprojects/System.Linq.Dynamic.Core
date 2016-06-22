688e49af32e5d6fc004de3634cc9413c6ef1c30d
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2016-06-22 09:00:55 +0200

    Add Include test
    Add Include test

---
+++ test/System.Linq.Dynamic.Core.Tests/EntitiesTests.Include.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Linq.Dynamic.Core.Tests.Helpers.Entities;
+#if EFCORE
+using Microsoft.EntityFrameworkCore;
+#else
+using System.Data.Entity;
+#endif
+using Xunit;
+
+namespace System.Linq.Dynamic.Core.Tests
+{
+    public partial class EntitiesTests : IDisposable
+    {
+        /// <summary>
+        /// Test for https://github.com/StefH/System.Linq.Dynamic.Core/issues/28
+        /// </summary>
+        [Fact]
+        public void Entities_Where_Include()
+        {
+            // Arrange
+            PopulateTestData(5, 2);
+
+            var expected = _context.Blogs.Include(b => b.Posts).Where(b => b.BlogId > 2000).ToArray();
+
+            // Act
+            var test = _context.Blogs.Include(b => b.Posts).Where("BlogId > 2000").ToArray();
+
+            // Assert
+            Assert.Equal(expected, test);
+        }
+    }
+}
