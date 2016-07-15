e8c1391bf07fee5c89e0906709dfdebc1e727a08
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2016-07-15 20:04:02 +0200

    #21 : update some Distinct tests

--- test/System.Linq.Dynamic.Core.Tests/QueryableTests.Distinct.cs
+++ test/System.Linq.Dynamic.Core.Tests/QueryableTests.Distinct.cs
@@ -1,11 +1,13 @@
-﻿using Xunit;
+﻿using System.Collections.Generic;
+using System.Linq.Dynamic.Core.Tests.Helpers.Models;
+using Xunit;
 
 namespace System.Linq.Dynamic.Core.Tests
 {
     public partial class QueryableTests
     {
         [Fact]
-        public void Distinct_Basic()
+        public void Distinct()
         {
             //Arrange
             var list = new[] { 1, 2, 2, 3 };
@@ -20,7 +22,7 @@ namespace System.Linq.Dynamic.Core.Tests
         }
 
         [Fact]
-        public void Distinct_Basic_Dynamic()
+        public void Distinct_Dynamic_1()
         {
             //Arrange
             var list = new[]
@@ -33,6 +35,27 @@ namespace System.Linq.Dynamic.Core.Tests
             //Act
             var expected = list.Select(l => l.list.Distinct()).ToArray<object>();
             var result = queryable.Select("list.Distinct()").ToDynamicArray();
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Distinct_Dynamic_2()
+        {
+            //Arrange
+            var list = new[]
+            {
+                new User { UserName = "a", Income = 1 },
+                new User { UserName = "a", Income = 2 },
+                new User { UserName = "b", Income = 1 }
+            };
+
+            //Act
+            var expected = list.Select(u => u.UserName).Distinct().ToArray();
+
+            IQueryable queryable = list.AsQueryable();
+            var result = queryable.Select("UserName").Distinct().ToDynamicArray<string>();
 
             //Assert
             Assert.Equal(expected, result);
