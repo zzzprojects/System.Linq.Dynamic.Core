bfcda8e112d87311ef1e33b5cb665d00a023c492
David Cizek <DavidCizek@seznam.cz>
2017-04-04 14:11:10 +0200

    Fix - when method has object parameter and ValueType value is passed into this method, result is exception in System.Dynamic.Utils.ExpressionUtils.ValidateOneArgument. Tests included. Without changes in ExpressionParser next two tests failed: ExpressionTests_MethodCall_ValueTypeToObjectParameterWithoutCast, ExpressionTests_MethodCall_NullableValueTypeToObjectParameter

---
+++ test/System.Linq.Dynamic.Core.Tests/Helpers/Models/Methods.cs
@@ -0,0 +1,26 @@
+﻿namespace System.Linq.Dynamic.Core.Tests.Helpers.Models
+{
+    public class Methods
+    {
+        public bool Method1(int value)
+        {
+            return value == 1;
+        }
+
+        public bool Method2(object value)
+        {
+            return value != null && (int)value == 1;
+        }
+
+        public bool Method3(object value)
+        {
+            Item item = value as Item;
+            return item != null && item.Value == 1;
+        }
+
+        public class Item
+        {
+            public int Value { get; set; }
+        }
+    }
+}
