39e50ee7694bf88c3537a753573b8f4c760255ff
jogibear9988 <jochen.kuehner@gmx.de>
2017-05-16 09:36:29 +0200

    Unit Tests for Last and Single

--- test/System.Linq.Dynamic.Core.Tests/EntitiesTests.Count.cs
+++ test/System.Linq.Dynamic.Core.Tests/EntitiesTests.Count.cs
@@ -6,7 +6,7 @@ namespace System.Linq.Dynamic.Core.Tests
     public partial class EntitiesTests
     {
         [Fact]
-        public void Entities_Count_Predicate()
+        public void Entities_Last_Predicate()
         {
             const string search = "a";
 
@@ -18,8 +18,68 @@ namespace System.Linq.Dynamic.Core.Tests
             _context.SaveChanges();
 
             //Act
-            int expected = _context.Blogs.Count(b => b.Name.Contains(search));
-            int result = _context.Blogs.Count("Name.Contains(@0)", search);
+            var expected = _context.Blogs.Last(b => b.Name.Contains(search));
+            var result = _context.Blogs.Last("Name.Contains(@0)", search);
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Entities_LastOrDefault_Predicate()
+        {
+            const string search = "a";
+
+            //Arrange
+            var blog1 = new Blog { Name = "blog a", BlogId = 1000 };
+            var blog2 = new Blog { Name = "blog b", BlogId = 3000 };
+            _context.Blogs.Add(blog1);
+            _context.Blogs.Add(blog2);
+            _context.SaveChanges();
+
+            //Act
+            var expected = _context.Blogs.LastOrDefault(b => b.Name.Contains(search));
+            var result = _context.Blogs.LastOrDefault("Name.Contains(@0)", search);
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Entities_Single_Predicate()
+        {
+            const string search = "a";
+
+            //Arrange
+            var blog1 = new Blog { Name = "blog a", BlogId = 1000 };
+            var blog2 = new Blog { Name = "blog b", BlogId = 3000 };
+            _context.Blogs.Add(blog1);
+            _context.Blogs.Add(blog2);
+            _context.SaveChanges();
+
+            //Act
+            var expected = _context.Blogs.Single(b => b.Name.Contains(search));
+            var result = _context.Blogs.Single("Name.Contains(@0)", search);
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Entities_SingleOrDefault_Predicate()
+        {
+            const string search = "a";
+
+            //Arrange
+            var blog1 = new Blog { Name = "blog a", BlogId = 1000 };
+            var blog2 = new Blog { Name = "blog b", BlogId = 3000 };
+            _context.Blogs.Add(blog1);
+            _context.Blogs.Add(blog2);
+            _context.SaveChanges();
+
+            //Act
+            var expected = _context.Blogs.SingleOrDefault(b => b.Name.Contains(search));
+            var result = _context.Blogs.SingleOrDefault("Name.Contains(@0)", search);
 
             //Assert
             Assert.Equal(expected, result);
