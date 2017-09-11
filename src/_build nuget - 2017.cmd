52f24b7383d1d33d6fb4f433dce2e02490547a8c
Stef Heyenrath <stef.heyenrath@gmail.com>
2017-09-11 18:52:54 +0200

    IncludeSymbols (fixed #104)
        <DebugType>full</DebugType> 
        <IncludeSource>True</IncludeSource> 
        <IncludeSymbols>True</IncludeSymbols>

--- src/_build nuget - 2017.cmd
+++ src/_build nuget - 2017.cmd
@@ -1,4 +1,3 @@
-rem dotnet restore System.Linq.Dynamic.Core\System.Linq.Dynamic.Core.csproj
 "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\msbuild.exe" System.Linq.Dynamic.Core\System.Linq.Dynamic.Core.csproj /p:Configuration=Release
 "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\msbuild.exe" EntityFramework.DynamicLinq\EntityFramework.DynamicLinq.csproj /p:Configuration=Release
 "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\msbuild.exe" Microsoft.EntityFrameworkCore.DynamicLinq\Microsoft.EntityFrameworkCore.DynamicLinq.csproj /p:Configuration=Release
--- src/_build nuget.cmd
+++ src/_build nuget.cmd
@@ -1,5 +1,4 @@
-dotnet restore
-dotnet pack -c Release System.Linq.Dynamic.Core\project.json
-dotnet pack -c Release EntityFramework.DynamicLinq\project.json
-dotnet pack -c Release Microsoft.EntityFrameworkCore.DynamicLinq\project.json
+dotnet pack -c Release System.Linq.Dynamic.Core\System.Linq.Dynamic.Core.csproj --include-symbols
+dotnet pack -c Release EntityFramework.DynamicLinq\EntityFramework.DynamicLinq.csproj --include-symbols
+dotnet pack -c Release Microsoft.EntityFrameworkCore.DynamicLinq\Microsoft.EntityFrameworkCore.DynamicLinq.csproj --include-symbols
 pause
