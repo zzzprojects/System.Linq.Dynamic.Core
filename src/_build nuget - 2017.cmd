54156574df38aa88955e77d84b597ab653cf7fcd
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2017-11-01 22:02:55 +0100

    1.0.7.11

--- src/_build nuget - 2017.cmd
+++
@@ -1,4 +0,0 @@
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\msbuild.exe" System.Linq.Dynamic.Core\System.Linq.Dynamic.Core.csproj /p:Configuration=Release
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\msbuild.exe" EntityFramework.DynamicLinq\EntityFramework.DynamicLinq.csproj /p:Configuration=Release
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\msbuild.exe" Microsoft.EntityFrameworkCore.DynamicLinq\Microsoft.EntityFrameworkCore.DynamicLinq.csproj /p:Configuration=Release
-pause
