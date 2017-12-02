6cb2a854a259c2b7615929227a8abe17cbf1604d
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2017-12-02 16:55:55 +0100

    refcatored

--- src/System.Linq.Dynamic.Core/ParsingConfig.cs
+++ src/System.Linq.Dynamic.Core/ParsingConfig.cs
@@ -3,16 +3,18 @@
 namespace System.Linq.Dynamic.Core
 {
     /// <summary>
-    /// Static configuration class for Dynamic Linq.
+    /// Configuration class for Dynamic Linq.
     /// </summary>
-    public static class GlobalConfig
+    public class ParsingConfig
     {
-        private static IDynamicLinkCustomTypeProvider _customTypeProvider;
+        internal static ParsingConfig Default { get; } = new ParsingConfig();
+
+        private IDynamicLinkCustomTypeProvider _customTypeProvider;
 
         /// <summary>
         /// Gets or sets the <see cref="IDynamicLinkCustomTypeProvider"/>.
         /// </summary>
-        public static IDynamicLinkCustomTypeProvider CustomTypeProvider
+        public IDynamicLinkCustomTypeProvider CustomTypeProvider
         {
             get
             {
@@ -29,31 +31,15 @@ namespace System.Linq.Dynamic.Core
                 if (_customTypeProvider != value)
                 {
                     _customTypeProvider = value;
-
-                    ExpressionParser.ResetDynamicLinqTypes();
                 }
             }
         }
-
-        static bool _contextKeywordsEnabled = true;
 
         /// <summary>
         /// Determines if the context keywords (it, parent, and root) are valid and usable inside a Dynamic Linq string expression.  
         /// Does not affect the usability of the equivalent context symbols ($, ^ and ~).
         /// </summary>
-        public static bool AreContextKeywordsEnabled
-        {
-            get => _contextKeywordsEnabled;
-            set
-            {
-                if (value != _contextKeywordsEnabled)
-                {
-                    _contextKeywordsEnabled = value;
-
-                    ExpressionParser.ResetDynamicLinqTypes();
-                }
-            }
-        }
+        public bool AreContextKeywordsEnabled { get; set; } = true;
 
         /// <summary>
         /// Gets or sets a value indicating whether to use dynamic object class for anonymous types.
@@ -61,6 +47,6 @@ namespace System.Linq.Dynamic.Core
         /// <value>
         /// <c>true</c> if wether to use dynamic object class for anonymous types; otherwise, <c>false</c>.
         /// </value>
-        public static bool UseDynamicObjectClassForAnonymousTypes { get; set; }
+        public bool UseDynamicObjectClassForAnonymousTypes { get; set; }
     }
 }
