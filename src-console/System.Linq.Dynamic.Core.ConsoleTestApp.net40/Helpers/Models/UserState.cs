4b7d15a0f9c4869c8904db7ba296e31466a872e9
Oleg Nadymov <o.nadymov@cashmere.ru>
2018-08-02 00:33:49 +0300

    Add references for missing classes

---
+++ src-console/System.Linq.Dynamic.Core.ConsoleTestApp.net40/Helpers/Models/UserState.cs
@@ -0,0 +1,10 @@
+﻿namespace System.Linq.Dynamic.Core.Tests.Helpers.Models
+{
+    public class UserState
+    {
+        public Guid StatusCode { get; set; }
+        public string Description { get; set; }
+
+        public static implicit operator Guid(UserState state) => state?.StatusCode ?? Guid.Empty;
+    }
+}
