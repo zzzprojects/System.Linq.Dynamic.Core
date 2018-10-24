51f95f2a13adb35c9faac59d096c6b16794dbc66
sspekinc <15963470+sspekinc@users.noreply.github.com>
2018-10-24 11:58:04 +0300

    Update ExpressionHelper.cs

--- src/System.Linq.Dynamic.Core/Parser/ExpressionHelper.cs
+++ src/System.Linq.Dynamic.Core/Parser/ExpressionHelper.cs
@@ -232,7 +232,7 @@ namespace System.Linq.Dynamic.Core.Parser
 
                 if (constantExpression.Type == typeof(string)) 
                 {
-                    expression = WrappedConstant((string)constantExpression.Value);
+                    expression = WrappedConstant((string)constantExpression.ToString());
                 }
                 else if (constantExpression.Type == typeof(long)) 
                 {
