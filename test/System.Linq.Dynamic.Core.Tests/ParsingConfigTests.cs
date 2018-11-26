71bc716f459b75dd63c577fb1b05214bf4c59c99
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2018-11-26 20:03:06 +0100

    ParsingConfigTests

---
+++ test/System.Linq.Dynamic.Core.Tests/ParsingConfigTests.cs
@@ -0,0 +1,43 @@
+﻿using NFluent;
+using Xunit;
+
+namespace System.Linq.Dynamic.Core.Tests
+{
+    public class ParsingConfigTests
+    {
+        class TestQueryableAnalyzer : IQueryableAnalyzer
+        {
+            public bool SupportsLinqToObjects(IQueryable query, IQueryProvider provider)
+            {
+                return true;
+            }
+        }
+
+        [Fact]
+        public void ParsingConfig_QueryableAnalyzer_Set_Null()
+        {
+            // Assign
+            var config = ParsingConfig.Default;
+
+            // Act
+            config.QueryableAnalyzer = null;
+
+            // Assert
+            Check.That(config.QueryableAnalyzer).IsNotNull();
+        }
+
+        [Fact]
+        public void ParsingConfig_QueryableAnalyzer_Set_Custom()
+        {
+            // Assign
+            var config = ParsingConfig.Default;
+            var analyzer = new TestQueryableAnalyzer();
+
+            // Act
+            config.QueryableAnalyzer = analyzer;
+
+            // Assert
+            Check.That(config.QueryableAnalyzer).IsEqualTo(analyzer);
+        }
+    }
+}
