823065d7701fe5cb8e511993b228eac403fa5662
Stef Heyenrath <stef.heyenrath@gmail.com>
2020-01-16 09:26:42 +0000

    Add docfx

---
+++ docfx/build-docs.ps1
@@ -0,0 +1,36 @@
+<#
+.SYNOPSIS
+    Builds documentation for this project into static HTML output
+
+.NOTES
+    Author           : Stef Heyenrath - @sheyenrath
+
+.LINK
+    https://github.com/StefH/System.Linq.Dynamic.Core/docfx
+	https://raw.githubusercontent.com/jsiegmund/docfx-seed/master/build-docs.ps1	
+#>
+
+# For running this script: "Set-ExecutionPolicy Bypass"
+
+param (
+  [Switch]$Serve = $false
+)
+
+# Set variables
+$scriptsPath = $PSScriptRoot
+
+Write-Host "Starting docfx build process." -ForegroundColor Yellow
+
+$docfxArgs = @(    
+  "docfx.json"
+)
+
+# Add '--serve' to arguments in order to start serving the site in place
+if ($Serve.IsPresent) {
+
+  Write-Output "Serving website from '_site'"
+  $docfxArgs = $docfxArgs + "--serve"
+}
+
+# Start the make process which uses Sphynx to convert RST to HTML
+Start-Process "docfx" -ArgumentList $docfxArgs -NoNewWindow -Wait
---
+++ docfx/index.md
@@ -0,0 +1,14 @@
+# System.Linq.Dynamic.Core
+This is a **.NET Core / Standard port** of the Microsoft assembly for the .Net 4.0 Dynamic language functionality.
+
+# Overview
+With this library it's possible to write Dynamic LINQ queries (string based) on an `IQueryable`:
+``` c#
+var query = db.Customers
+    .Where("City == @0 and Orders.Count >= @1", "London", 10)
+    .OrderBy("CompanyName")
+    .Select("new(CompanyName as Name, Phone)");
+```
+
+# API Documentation
+See [link](api/System.Linq.Dynamic.Core.html)
