238df2132c9463cdd1356e42221be0ddfdb34cd3
countincognito <countincognito@outlook.com>
2020-02-25 16:57:26 +0000

    Add ConsoleApp_netcore2.1_CosmosDb test console.

---
+++ src-console/ConsoleApp_netcore2.1_CosmosDb/Entities/EntityData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleAppCosmosDb.Entities
+{
+    [Serializable]
+    public class EntityData
+    {
+        public IList<EntityReading> Readings { get; set; }
+    }
+}
