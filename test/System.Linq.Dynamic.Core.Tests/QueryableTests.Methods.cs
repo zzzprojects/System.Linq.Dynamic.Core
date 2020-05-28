803ee4dd3aca85371d6f9629ab46489cf3e07c5e
Stef Heyenrath <stef.heyenrath@gmail.com>
2020-05-28 07:24:10 +0000

    Fixed : calling methods which returns a nullable (#382)

---
+++ test/System.Linq.Dynamic.Core.Tests/QueryableTests.Methods.cs
@@ -0,0 +1,64 @@
+﻿using Xunit;
+
+namespace System.Linq.Dynamic.Core.Tests
+{
+    public partial class QueryableTests
+    {
+        [Fact]
+        public void CallMethod()
+        {
+            // Arrange
+            var query = new[] { new Test() }.AsQueryable();
+
+            // Act
+            var result = query.Select<decimal>("t => t.GetDecimal()").First();
+
+            // Assert
+            Assert.Equal(42, result);
+        }
+
+        [Fact]
+        public void CallMethodWhichReturnsNullable()
+        {
+            // Arrange
+            var query = new[] { new Test() }.AsQueryable();
+
+            // Act
+            var result = query.Select<decimal?>("t => t.GetNullableDecimal()").First();
+
+            // Assert
+            Assert.Equal(null, result);
+        }
+
+        [Fact]
+        public void CallMethodWhichReturnsNullable_WithValue()
+        {
+            // Arrange
+            var query = new[] { new Test() }.AsQueryable();
+
+            // Act
+            var result = query.Select<decimal?>("t => t.GetNullableDecimalWithValue()").First();
+
+            // Assert
+            Assert.Equal(100, result);
+        }
+    }
+
+    class Test
+    {
+        public decimal GetDecimal()
+        {
+            return 42;
+        }
+
+        public decimal? GetNullableDecimal()
+        {
+            return null;
+        }
+
+        public decimal? GetNullableDecimalWithValue()
+        {
+            return 100;
+        }
+    }
+}
