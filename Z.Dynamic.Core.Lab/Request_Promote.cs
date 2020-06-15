ee5b57c474bd08399fb1490b1e31e64a1373a549
Jonathan Magnan <info@zzzprojects.com>
2020-06-15 15:06:16 -0400

    Added Promote lab

---
+++ Z.Dynamic.Core.Lab/Request_Promote.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Dynamic.Core;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Z.Dynamic.Core.Lab
+{
+    public class Request_Promote
+    {
+        public static void Execute()
+        {
+            var strArray = new[] { "1", "2", "3", "4" };
+            var x = new List<ParameterExpression>();
+            x.Add(Expression.Parameter(strArray.GetType(), "strArray"));
+
+            var config = new ParsingConfig();
+            string query = "string.Join(\",\" , strArray)";
+
+            var e = DynamicExpressionParser.ParseLambda(config, x.ToArray(), null, query);
+            Delegate del = e.Compile();
+            var result = del.DynamicInvoke(strArray);
+
+            //var intArray = new[] { 1, 2, 3, 4 };
+            //var x = new List<ParameterExpression>();
+            //x.Add(Expression.Parameter(intArray.GetType(), "intArray"));
+
+            //var config = new ParsingConfig();
+            //string query = "string.Join(\",\" , intArray)";
+
+            //var e = DynamicExpressionParser.ParseLambda(config, x.ToArray(), null, query);
+            //Delegate del = e.Compile();
+            //var result = del.DynamicInvoke(intArray);
+        }
+    }
+}
