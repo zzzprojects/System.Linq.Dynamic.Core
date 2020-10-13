37bd0e2d016e7b0e73444e7895d187b584d3e980
Mikaël Lanouette <mikael.lanouette@gmail.com>
2020-10-13 13:17:20 -0400

    save

---
+++ Z.Dynamic.Core.Lab/Request_LambdaAddFunc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Dynamic.Core;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Z.Dynamic.Core.Lab
+{
+    class Request_LambdaAddFunc
+    {
+        public static void Execute()
+        {
+            var expression = (Action<int>)DynamicExpressionParser.ParseLambda(typeof(Action<int>), new[] { Expression.Parameter(typeof(int), "x") }, typeof(int),  "x + 1").Compile();
+            var expression2 = (Func<int, int>)DynamicExpressionParser.ParseLambda(typeof(Func<int, int>), new[] { Expression.Parameter(typeof(int), "x") }, typeof(int), "x + 1").Compile();
+
+            expression(3);
+        }
+    }
+}
