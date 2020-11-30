4b1b99700e35fd9c557f99b33ec90b280299298e
Stef Heyenrath <stef.heyenrath@gmail.com>
2020-11-30 20:33:29 +0100

    a

---
+++ src-android/TestSLDC/LambdaExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq.Dynamic.Core;
+
+namespace TestSLDC
+{
+    internal static class LambdaExtensions
+    {
+        public static Func<T1, T2> ParseLambda<T1, T2>(this string expression)
+        {
+            return (Func<T1, T2>)DynamicExpressionParser.ParseLambda(false, typeof(T1), typeof(T2), expression).Compile();
+        }
+    }
+}
