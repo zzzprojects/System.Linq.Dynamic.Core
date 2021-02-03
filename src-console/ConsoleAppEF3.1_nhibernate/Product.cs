4fa6823b9ba97ec26f4f0ca901d7082551435a6b
Stef Heyenrath <stef.heyenrath@gmail.com>
2021-02-03 16:55:00 +0000

    add example

---
+++ src-console/ConsoleAppEF3.1_nhibernate/Product.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ConsoleAppEF3_1_nhibernate
+{
+	class Product
+	{
+		public virtual Guid Id { get; set; }
+		public virtual string Name { get; set; }
+		public virtual dynamic Properties { get; set; }
+	}
+}
