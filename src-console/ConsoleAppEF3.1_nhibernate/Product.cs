0b64918196f1498e0051e4a7d3192df455692af4
Stef Heyenrath <stef.heyenrath@gmail.com>
2021-02-04 12:22:23 +0000

    117

---
+++ src-console/ConsoleAppEF3.1_nhibernate/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleAppEF3_1_nhibernate
+{
+	class Product
+	{
+		public virtual Guid Id { get; set; }
+		public virtual string Name { get; set; }
+		public virtual dynamic Properties { get; set; }
+	}
+}
