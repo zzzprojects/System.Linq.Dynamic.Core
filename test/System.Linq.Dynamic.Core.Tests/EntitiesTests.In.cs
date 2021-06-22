95b8c47c9d3a563b0d524fe847615921d9a10271
Francois Botha <igitur@gmail.com>
2021-06-22 09:54:41 +0200

    Parse in operator before and/or

---
+++ test/System.Linq.Dynamic.Core.Tests/EntitiesTests.In.cs
@@ -0,0 +1,34 @@
+﻿#if EFCORE
+using Microsoft.EntityFrameworkCore;
+#else
+
+using System.Data.Entity;
+
+#endif
+
+using Xunit;
+
+namespace System.Linq.Dynamic.Core.Tests
+{
+    public partial class EntitiesTests : IDisposable
+    {
+        /// <summary>
+        /// Test for https://github.com/zzzprojects/System.Linq.Dynamic.Core/pull/524
+        /// </summary>
+        [Fact]
+        public void Entities_Where_In_And()
+        {
+            // Arrange
+            PopulateTestData();
+
+            var expected = _context.Blogs.Include(b => b.Posts).Where(b => new[] { 1, 3, 5 }.Contains(b.BlogId) && new [] { "Blog3", "Blog4" }.Contains(b.Name)).ToArray();
+
+            // Act
+            var test = _context.Blogs.Include(b => b.Posts).Where(@"BlogId in (1, 3, 5) and Name in (""Blog3"", ""Blog4"")").ToArray();
+
+            // Assert
+            Assert.NotEmpty(test);
+            Assert.Equal(expected, test);
+        }
+    }
+}
