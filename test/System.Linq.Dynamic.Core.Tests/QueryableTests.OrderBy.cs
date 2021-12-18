e32ccb3f8f21eea3e5e467bb6d245b02b65b0148
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2021-12-18 11:03:14 +0100

    Fix NET35 build + add unit test for OrderBy with IComparer (#561)

--- test/System.Linq.Dynamic.Core.Tests/QueryableTests.OrderBy.cs
+++ test/System.Linq.Dynamic.Core.Tests/QueryableTests.OrderBy.cs
@@ -7,18 +7,33 @@ namespace System.Linq.Dynamic.Core.Tests
     public partial class QueryableTests
     {
         [Fact]
+        public void OrderBy_Dynamic_IComparer()
+        {
+            // Arrange
+            var testList = User.GenerateSampleModels(2);
+            var qry = testList.AsQueryable();
+
+            // Act
+            var orderBy = testList.OrderBy(x => x.UserName, StringComparer.OrdinalIgnoreCase).ToArray();
+            var orderByDynamic = qry.OrderBy("UserName", StringComparer.OrdinalIgnoreCase).ToArray();
+
+            // Assert
+            Assert.Equal(orderBy, orderByDynamic);
+        }
+
+        [Fact]
         public void OrderBy_Dynamic()
         {
-            //Arrange
+            // Arrange
             var testList = User.GenerateSampleModels(100);
             var qry = testList.AsQueryable();
 
-            //Act
+            // Act
             var orderById = qry.OrderBy("Id");
             var orderByAge = qry.OrderBy("Profile.Age");
             var orderByComplex1 = qry.OrderBy("Profile.Age, Id");
 
-            //Assert
+            // Assert
             Assert.Equal(testList.OrderBy(x => x.Id).ToArray(), orderById.ToArray());
             Assert.Equal(testList.OrderBy(x => x.Profile.Age).ToArray(), orderByAge.ToArray());
             Assert.Equal(testList.OrderBy(x => x.Profile.Age).ThenBy(x => x.Id).ToArray(), orderByComplex1.ToArray());
@@ -27,26 +42,26 @@ namespace System.Linq.Dynamic.Core.Tests
         [Fact]
         public void OrderBy_Dynamic_AsStringExpression()
         {
-            //Arrange
+            // Arrange
             var testList = User.GenerateSampleModels(100);
             var qry = testList.AsQueryable();
 
-            //Act
+            // Act
             var expected = qry.SelectMany(x => x.Roles.OrderBy(y => y.Name)).Select(x => x.Name);
             var orderById = qry.SelectMany("Roles.OrderBy(Name)").Select("Name");
 
-            //Assert
+            // Assert
             Assert.Equal(expected.ToArray(), orderById.Cast<string>().ToArray());
         }
 
         [Fact]
         public void OrderBy_Dynamic_Exceptions()
         {
-            //Arrange
+            // Arrange
             var testList = User.GenerateSampleModels(100, allowNullableProfiles: true);
             var qry = testList.AsQueryable();
 
-            //Act
+            // Act
             Assert.Throws<ParseException>(() => qry.OrderBy("Bad=3"));
             Assert.Throws<ParseException>(() => qry.Where("Id=123"));
 
