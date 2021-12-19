886187ae7642e5958a335a75def9740a475b241b
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2021-12-19 10:11:10 +0100

    Make TextParser public (#562)

--- src/System.Linq.Dynamic.Core/Tokenizer/Token.cs
+++ src/System.Linq.Dynamic.Core/Tokenizer/Token.cs
@@ -1,13 +1,28 @@
 ﻿namespace System.Linq.Dynamic.Core.Tokenizer
 {
-    internal struct Token
+    /// <summary>
+    /// Token
+    /// </summary>
+    public struct Token
     {
+        /// <summary>
+        /// The TokenId.
+        /// </summary>
         public TokenId Id { get; set; }
 
+        /// <summary>
+        /// The Original TokenId.
+        /// </summary>
         public TokenId OriginalId { get; set; }
 
+        /// <summary>
+        /// The text.
+        /// </summary>
         public string Text { get; set; }
 
+        /// <summary>
+        /// The position.
+        /// </summary>
         public int Pos { get; set; }
     }
 }
