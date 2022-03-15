cb4a100822157121f8f29332c2c4a739408997ac
Stef Heyenrath <stef.heyenrath@gmail.com>
2022-03-15 19:06:44 +0100

    Support Struct for DynamicLinqTypeAttribute

--- src/System.Linq.Dynamic.Core/CustomTypeProviders/DynamicLinqTypeAttribute.cs
+++ src/System.Linq.Dynamic.Core/CustomTypeProviders/DynamicLinqTypeAttribute.cs
@@ -3,7 +3,7 @@
     /// <summary>
     /// Indicates to Dynamic Linq to consider the Type as a valid dynamic linq type.
     /// </summary>
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Enum, AllowMultiple = false, Inherited = false)]
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Enum, AllowMultiple = false, Inherited = false)]
     public sealed class DynamicLinqTypeAttribute : Attribute
     {
     }
