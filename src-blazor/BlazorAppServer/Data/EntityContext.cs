ca816879778f01d57d75b4af6373dde6aef8f712
Stef Heyenrath <stef.heyenrath@gmail.com>
2022-08-02 13:53:13 +0200

    BlazorAppServer Example

---
+++ src-blazor/BlazorAppServer/Data/EntityContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlazorAppServer.Data;
+
+public class EntityContext : DbContext
+{
+    public virtual DbSet<Contract> Contracts { get; set; }
+
+    public EntityContext()
+    {
+
+    }
+
+    public EntityContext(DbContextOptions<EntityContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Contracts;Integrated Security=True;Connect Timeout=30;TrustServerCertificate=True");
+        }
+    }
+}
---
+++ src-blazor/BlazorAppServer/Program.cs
@@ -0,0 +1,30 @@
+using BlazorAppServer.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
+//builder.Services.AddDbContext<EntityContext>(option => option.UseInMemoryDatabase("x"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
