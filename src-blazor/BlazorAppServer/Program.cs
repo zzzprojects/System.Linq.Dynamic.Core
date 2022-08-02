15b015e5ffe26f74a38b8ebcbcf29b692b56db05
Stef Heyenrath <stef.heyenrath@gmail.com>
2022-08-02 21:13:24 +0200

    .

--- src-blazor/BlazorAppServer/Program.cs
+++ src-blazor/BlazorAppServer/Program.cs
@@ -1,15 +1,10 @@
-using BlazorAppServer.Data;
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.EntityFrameworkCore;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 
-//builder.Services.AddDbContext<EntityContext>(option => option.UseInMemoryDatabase("x"));
+builder.Services.AddLocalization();
 
 var app = builder.Build();
 
@@ -19,12 +14,13 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error");
 }
 
-
 app.UseStaticFiles();
 
 app.UseRouting();
 
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
+
+app.UseRequestLocalization("fr-FR");
 
 app.Run();
