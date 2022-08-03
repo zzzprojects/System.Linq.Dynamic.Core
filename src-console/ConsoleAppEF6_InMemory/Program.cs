3242cb9b292c54571b5286b31903281a9859c6a3
Stef Heyenrath <stef.heyenrath@gmail.com>
2022-08-03 23:13:05 +0200

    wip

--- src-console/ConsoleAppEF6_InMemory/Program.cs
+++ src-console/ConsoleAppEF6_InMemory/Program.cs
@@ -3,31 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Dynamic.Core;
 using System.Text.Json;
+using System.Threading.Tasks;
 using ConsoleAppEF2.Database;
+using Microsoft.EntityFrameworkCore;
 
-namespace ConsoleApp_net5_0_EF6_InMemory
+namespace ConsoleApp_net5_0_EF6_InMemory;
+
+static class Program
 {
-    static class Program
-    {
-        private readonly static JsonSerializerOptions JsonSerializerOptions = new JsonSerializerOptions { WriteIndented = true };
+    private static readonly JsonSerializerOptions JsonSerializerOptions = new() { WriteIndented = true };
 
-        static void Main(string[] args)
+    static async Task Main(string[] args)
+    {
+        await using (var context = new TestContextEF6())
         {
-            using (var context = new TestContextEF6())
+            context.Products.Add(new ProductDynamic { NullableInt = 1, Dict = new Dictionary<string, object> { { "Name", "test" } } });
+            await context.SaveChangesAsync();
+        }
+
+        await using (var context = new TestContextEF6())
+        {
+            var resultsNormal = context.Products.Where(p => p.Dict["Name"] == "test").ToListAsync();
+            
+            var results1 = await context.Products.Where("Dict.Name == @0", "test").ToListAsync();
+            Console.WriteLine("results1:");
+            foreach (var result in results1)
             {
-                context.Products.Add(new ProductDynamic { Dict = new Dictionary<string, object> { { "Name", "test" } } });
-                context.SaveChanges();
+                Console.WriteLine(result.Key + ":" + JsonSerializer.Serialize(result.Dict, JsonSerializerOptions));
             }
 
-            using (var context = new TestContextEF6())
+            var results2 = await context.Products.Where("Dict.Name == @0", "test").ToDynamicListAsync();
+            Console.WriteLine("results2:");
+            foreach (var result in results2)
             {
-                var results = context.Products.Where("Dict.Name == @0", "test");
+                Console.WriteLine(result.Key + ":" + JsonSerializer.Serialize(result.Dict, JsonSerializerOptions));
+            }
 
-                Console.WriteLine("results = ?");
-                foreach (var result in results)
-                {
-                    Console.WriteLine(result.Key + JsonSerializer.Serialize(result.Dict, JsonSerializerOptions));
-                }
+            var results3 = await context.Products.Where("NullableInt == 1").ToDynamicListAsync(typeof(ProductDynamic));
+            Console.WriteLine("results3:");
+            foreach (var result in results3)
+            {
+                Console.WriteLine(result.Key + ":" + JsonSerializer.Serialize(result.Dict, JsonSerializerOptions));
             }
         }
     }
--- test/System.Linq.Dynamic.Core.Tests/QueryableTests.ToList.cs
+++ test/System.Linq.Dynamic.Core.Tests/QueryableTests.ToList.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq.Dynamic.Core.Tests.Helpers.Models;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace System.Linq.Dynamic.Core.Tests
@@ -6,7 +7,7 @@ namespace System.Linq.Dynamic.Core.Tests
     public partial class QueryableTests
     {
         [Fact]
-        public void ToList_Dynamic()
+        public void ToDynamicList()
         {
             // Arrange
             var testList = User.GenerateSampleModels(51);
@@ -17,7 +18,23 @@ namespace System.Linq.Dynamic.Core.Tests
             var testResult = testListQry.OrderBy("Roles.ToList().First().Name").Select("Id");
 
             // Assert
-            Assert.Equal(realResult.ToArray(), testResult.ToDynamicArray().Cast<Guid>());
+            Assert.Equal(realResult.ToArray(), testResult.ToDynamicList().Cast<Guid>());
+        }
+
+        [Fact]
+        public async Task ToDynamicListAsync()
+        {
+            // Arrange
+            var testList = User.GenerateSampleModels(51);
+            IQueryable testListQry = testList.AsQueryable();
+
+            // Act
+            var realResult = testList.OrderBy(x => x.Roles.ToList().First().Name).Select(x => x.Id);
+            var testResult = testListQry.OrderBy("Roles.ToList().First().Name").Select("Id");
+
+            // Assert
+            var dynamicResult = await testResult.ToDynamicListAsync();
+            Assert.Equal(realResult.ToArray(), dynamicResult.Cast<Guid>());
         }
     }
 }
