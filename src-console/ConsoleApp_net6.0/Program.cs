840093ad91fe77071e2009b82b59b928124b88f0
Stef Heyenrath <stef.heyenrath@gmail.com>
2022-11-19 14:18:48 +0100

    .

--- src-console/ConsoleApp_net6.0/Program.cs
+++ src-console/ConsoleApp_net6.0/Program.cs
@@ -1,17 +1,35 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Dynamic.Core;
 
 namespace ConsoleApp_net6._0
 {
+    public class X
+    {
+        public string Key { get; set; } = null!;
+
+        public List<Y>? Contestants { get; set; }
+    }
+
+    public class Y
+    {
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            var q = new[]
+            {
+                new X { Key = "x" },
+                new X { Key = "a" },
+                new X { Key = "a", Contestants = new List<Y> { new Y() } }
+            }.AsQueryable();
+            var groupByKey = q.GroupBy("Key");
+            var selectQry = groupByKey.Select("new (Key, Sum(np(Contestants.Count, 0)) As TotalCount)").ToDynamicList();
+
             Normal();
             Dynamic();
-
-            int y = 0;
         }
 
         private static void Normal()
