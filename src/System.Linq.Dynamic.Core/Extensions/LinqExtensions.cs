3b7ee3818ebecce2881d97b84aa0d5b8fa3780b8
Stef Heyenrath <stef.heyenrath@gmail.com>
2023-02-04 19:52:39 +0100

    Update AbstractDynamicLinqCustomTypeProvider

--- src/System.Linq.Dynamic.Core/Extensions/LinqExtensions.cs
+++ src/System.Linq.Dynamic.Core/Extensions/LinqExtensions.cs
@@ -1,13 +1,22 @@
 ﻿using System.Collections.Generic;
+using System.Linq.Dynamic.Core.Validation;
 
-namespace System.Linq.Dynamic.Core.Extensions
+namespace System.Linq.Dynamic.Core.Extensions;
+
+internal static class LinqExtensions
 {
-    internal static class LinqExtensions
+    // Ex: collection.TakeLast(5);
+    public static IEnumerable<T> TakeLast<T>(this IList<T> source, int n)
     {
-        // Ex: collection.TakeLast(5);
-        public static IEnumerable<T> TakeLast<T>(this IList<T> source, int n)
-        {
-            return source.Skip(Math.Max(0, source.Count() - n));
-        }
+        return source.Skip(Math.Max(0, source.Count() - n));
+    }
+
+    public static IEnumerable<T> WhereNotNull<T>(this IEnumerable<T?> sequence, Func<T, bool>? predicate = null)
+    {
+        Check.NotNull(sequence);
+
+        return sequence
+            .Where(e => e != null)
+            .Where(e => predicate != null && predicate(e!))!;
     }
 }
