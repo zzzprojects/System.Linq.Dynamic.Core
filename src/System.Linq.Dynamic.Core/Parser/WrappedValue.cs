9c47deb2f37606a0423d17d6aee4b0ec7b0d3652
Stef Heyenrath <stef.heyenrath@gmail.com>
2023-02-11 09:59:33 +0100

    .

--- src/System.Linq.Dynamic.Core/Parser/WrappedValue.cs
+++ src/System.Linq.Dynamic.Core/Parser/WrappedValue.cs
@@ -1,11 +1,91 @@
-﻿namespace System.Linq.Dynamic.Core.Parser;
+﻿using System.Collections.Generic;
 
-internal class WrappedValue<TValue>
+namespace System.Linq.Dynamic.Core.Parser
 {
-    public TValue Value { get; }
-
-    public WrappedValue(TValue value)
+    internal class WrappedValue<TValue>
     {
-        Value = value;
+        public TValue Value { get; }
+
+        public WrappedValue(TValue value)
+        {
+            Value = value;
+        }
+
+        public static bool operator ==(WrappedValue<TValue>? left, WrappedValue<TValue>? right)
+        {
+            if (ReferenceEquals(left, right))
+            {
+                return true;
+            }
+
+            if (ReferenceEquals(left, null) || ReferenceEquals(right, null))
+            {
+                return false;
+            }
+
+            return EqualityComparer<TValue>.Default.Equals(left.Value, right.Value);
+        }
+
+        public static bool operator !=(WrappedValue<TValue>? left, WrappedValue<TValue>? right)
+        {
+            return !(left == right);
+        }
+
+        public static bool operator ==(WrappedValue<TValue>? left, TValue? right)
+        {
+            if (ReferenceEquals(left, null))
+            {
+                return false;
+            }
+
+            return EqualityComparer<TValue>.Default.Equals(left.Value, right);
+        }
+
+        public static bool operator !=(WrappedValue<TValue>? left, TValue? right)
+        {
+            return !(left == right);
+        }
+
+        public static bool operator ==(TValue? left, WrappedValue<TValue>? right)
+        {
+            if (ReferenceEquals(right, null))
+            {
+                return false;
+            }
+
+            return EqualityComparer<TValue>.Default.Equals(left, right.Value);
+        }
+
+        public static bool operator !=(TValue? left, WrappedValue<TValue>? right)
+        {
+            return !(left == right);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            if (ReferenceEquals(obj, null))
+            {
+                return false;
+            }
+
+            WrappedValue<TValue>? other = obj as WrappedValue<TValue>;
+
+            if (ReferenceEquals(other, null))
+            {
+                return false;
+            }
+
+            return EqualityComparer<TValue>.Default.Equals(Value, other.Value);
+        }
+
+        public override int GetHashCode()
+        {
+            return Value?.GetHashCode() ?? 0;
+        }
     }
 }
---
+++ test/System.Linq.Dynamic.Core.Tests/Parser/WrappedValueTests.cs
@@ -0,0 +1,88 @@
+﻿using System.Linq.Dynamic.Core.Parser;
+using FluentAssertions;
+using Xunit;
+
+namespace System.Linq.Dynamic.Core.Tests.Parser;
+
+public class WrappedValueTests
+{
+    [Fact]
+    public void WrappedValue_OfTypeString_OperatorEquals_String()
+    {
+        // Arrange
+        var wrapped = new WrappedValue<string>("str");
+
+        // Act
+        var result1A = wrapped == "str";
+        var result1B = "str" == wrapped;
+        var result2A = wrapped == "x";
+        var result2B = "x" == wrapped;
+
+        // Assert
+        result1A.Should().BeTrue();
+        result1B.Should().BeTrue();
+        result2A.Should().BeFalse();
+        result2B.Should().BeFalse();
+    }
+
+    [Fact]
+    public void WrappedValue_OfTypeString_OperatorNotEquals_String()
+    {
+        // Arrange
+        var wrapped = new WrappedValue<string>("str");
+
+        // Act
+        var result1A = wrapped != "str";
+        var result1B = "str" != wrapped;
+        var result2A = wrapped == "x";
+        var result2B = "x" == wrapped;
+
+        // Assert
+        result1A.Should().BeFalse();
+        result1B.Should().BeFalse();
+        result2A.Should().BeFalse();
+        result2B.Should().BeFalse();
+    }
+
+    [Fact]
+    public void WrappedValue_OfTypeString_OperatorEquals_WrappedValue_OfTypeString()
+    {
+        // Arrange
+        var wrapped = new WrappedValue<string>("str");
+        var testEqual = new WrappedValue<string>("str");
+        var testNotEqual = new WrappedValue<string>("x");
+
+        // Act
+        var result1A = wrapped == testEqual;
+        var result1B = testEqual == wrapped;
+        var result2A = wrapped == testNotEqual;
+        var result2B = testNotEqual == wrapped;
+
+        // Assert
+        result1A.Should().BeTrue();
+        result1B.Should().BeTrue();
+        result2A.Should().BeFalse();
+        result2B.Should().BeFalse();
+    }
+
+    [Fact]
+    public void WrappedValue_OfTypeString_OperatorNotEquals_WrappedValue_OfTypeString()
+    {
+        // Arrange
+        var wrapped = new WrappedValue<string>("str");
+        var testEqual = new WrappedValue<string>("str");
+        var testNotEqual = new WrappedValue<string>("x");
+
+        // Act
+        var result1A = wrapped != testEqual;
+        var result1B = testEqual != wrapped;
+        var result2A = wrapped != testNotEqual;
+        var result2B = testNotEqual != wrapped;
+
+        // Assert
+        result1A.Should().BeFalse();
+        result1B.Should().BeFalse();
+        result2A.Should().BeTrue();
+        result2B.Should().BeTrue();
+    }
+}
