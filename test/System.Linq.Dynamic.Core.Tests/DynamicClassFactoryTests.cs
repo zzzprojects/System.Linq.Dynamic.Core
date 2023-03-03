c91a4de618c2a58f85ed06bd57dc616ab0312e80
Stef Heyenrath <stef.heyenrath@gmail.com>
2023-03-03 20:24:06 +0100

    Add unit test for issue 593

--- test/System.Linq.Dynamic.Core.Tests/DynamicClassFactoryTests.cs
+++ test/System.Linq.Dynamic.Core.Tests/DynamicClassFactoryTests.cs
@@ -3,88 +3,119 @@ using System.Collections.Generic;
 using FluentAssertions;
 using Xunit;
 
-namespace System.Linq.Dynamic.Core.Tests
+namespace System.Linq.Dynamic.Core.Tests;
+
+public class DynamicClassFactoryTests
 {
-    public class DynamicClassFactoryTests
+    [Fact]
+    public void CreateGenericComparerTypeForInt()
     {
-        [Fact]
-        public void CreateGenericComparerTypeForInt()
-        {
-            // Assign
-            var comparer = new CustomCaseInsensitiveComparer();
-            var comparerGenericType = typeof(IComparer<>).MakeGenericType(typeof(int));
-            var a = 1;
-            var b = 2;
+        // Assign
+        var comparer = new CustomCaseInsensitiveComparer();
+        var comparerGenericType = typeof(IComparer<>).MakeGenericType(typeof(int));
+        var a = 1;
+        var b = 2;
 
-            // Act
-            var type = DynamicClassFactory.CreateGenericComparerType(comparerGenericType, comparer.GetType());
+        // Act
+        var type = DynamicClassFactory.CreateGenericComparerType(comparerGenericType, comparer.GetType());
 
-            // Assert
-            var instance = (IComparer<int>)Activator.CreateInstance(type);
-            int greaterThan = instance.Compare(a, b);
-            greaterThan.Should().Be(1);
+        // Assert
+        var instance = (IComparer<int>)Activator.CreateInstance(type);
+        int greaterThan = instance.Compare(a, b);
+        greaterThan.Should().Be(1);
 
-            int equal = instance.Compare(a, a);
-            equal.Should().Be(0);
+        int equal = instance.Compare(a, a);
+        equal.Should().Be(0);
 
-            int lessThan = instance.Compare(b, a);
-            lessThan.Should().Be(-1);
-        }
-
-        [Fact]
-        public void CreateGenericComparerTypeForString()
-        {
-            // Assign
-            var comparer = new CustomCaseInsensitiveComparer();
-            var comparerGenericType = typeof(IComparer<>).MakeGenericType(typeof(string));
-            var a = "a";
-            var b = "b";
-
-            // Act
-            var type = DynamicClassFactory.CreateGenericComparerType(comparerGenericType, comparer.GetType());
-
-            // Assert
-            var instance = (IComparer<string>)Activator.CreateInstance(type);
-            int greaterThan = instance.Compare(a, b);
-            greaterThan.Should().Be(1);
-
-            int equal = instance.Compare(a, a);
-            equal.Should().Be(0);
-
-            int lessThan = instance.Compare(b, a);
-            lessThan.Should().Be(-1);
-        }
-
-        [Fact]
-        public void CreateGenericComparerTypeForDateTime()
-        {
-            // Assign
-            var comparer = new CustomCaseInsensitiveComparer();
-            var comparerGenericType = typeof(IComparer<>).MakeGenericType(typeof(DateTime));
-            var a = new DateTime(2022, 1, 1);
-            var b = new DateTime(2023, 1, 1);
-
-            // Act
-            var type = DynamicClassFactory.CreateGenericComparerType(comparerGenericType, comparer.GetType());
-
-            // Assert
-            var instance = (IComparer<DateTime>)Activator.CreateInstance(type);
-            int greaterThan = instance.Compare(a, b);
-            greaterThan.Should().Be(1);
-
-            int equal = instance.Compare(a, a);
-            equal.Should().Be(0);
-
-            int lessThan = instance.Compare(b, a);
-            lessThan.Should().Be(-1);
-        }
+        int lessThan = instance.Compare(b, a);
+        lessThan.Should().Be(-1);
     }
 
-    public class CustomCaseInsensitiveComparer : IComparer
+    [Fact]
+    public void CreateGenericComparerTypeForString()
     {
-        public int Compare(object x, object y)
-        {
-            return new CaseInsensitiveComparer().Compare(y, x);
-        }
+        // Assign
+        var comparer = new CustomCaseInsensitiveComparer();
+        var comparerGenericType = typeof(IComparer<>).MakeGenericType(typeof(string));
+        var a = "a";
+        var b = "b";
+
+        // Act
+        var type = DynamicClassFactory.CreateGenericComparerType(comparerGenericType, comparer.GetType());
+
+        // Assert
+        var instance = (IComparer<string>)Activator.CreateInstance(type);
+        int greaterThan = instance.Compare(a, b);
+        greaterThan.Should().Be(1);
+
+        int equal = instance.Compare(a, a);
+        equal.Should().Be(0);
+
+        int lessThan = instance.Compare(b, a);
+        lessThan.Should().Be(-1);
+    }
+
+    [Fact]
+    public void CreateGenericComparerTypeForDateTime()
+    {
+        // Assign
+        var comparer = new CustomCaseInsensitiveComparer();
+        var comparerGenericType = typeof(IComparer<>).MakeGenericType(typeof(DateTime));
+        var a = new DateTime(2022, 1, 1);
+        var b = new DateTime(2023, 1, 1);
+
+        // Act
+        var type = DynamicClassFactory.CreateGenericComparerType(comparerGenericType, comparer.GetType());
+
+        // Assert
+        var instance = (IComparer<DateTime>)Activator.CreateInstance(type);
+        int greaterThan = instance.Compare(a, b);
+        greaterThan.Should().Be(1);
+
+        int equal = instance.Compare(a, a);
+        equal.Should().Be(0);
+
+        int lessThan = instance.Compare(b, a);
+        lessThan.Should().Be(-1);
+    }
+
+[Fact]
+public void DynamicClassArray()
+{
+    // Arrange
+    var field = new
+    {
+        Name = "firstName",
+        Value = "firstValue"
+    };
+    var dynamicClasses = new List<DynamicClass>();
+
+    var props = new DynamicProperty[]
+    {
+        new DynamicProperty(field.Name, typeof(string))
+    };
+
+    var type = DynamicClassFactory.CreateType(props);
+
+    var dynamicClass = (DynamicClass) Activator.CreateInstance(type);
+    dynamicClass.SetDynamicPropertyValue(field.Name, field.Value);
+
+    dynamicClasses.Add(dynamicClass);
+
+    var query = dynamicClasses.AsQueryable();
+
+    // Act
+    var isValid = query.Any("firstName eq \"firstValue\"");
+
+    // Assert
+    isValid.Should().BeTrue();
+}
+}
+
+public class CustomCaseInsensitiveComparer : IComparer
+{
+    public int Compare(object x, object y)
+    {
+        return new CaseInsensitiveComparer().Compare(y, x);
     }
 }
--- test/System.Linq.Dynamic.Core.Tests/DynamicClassTest.cs
+++ test/System.Linq.Dynamic.Core.Tests/DynamicClassTest.cs
@@ -1,126 +1,124 @@
 ﻿using System.Collections.Generic;
-using System.Xml.Linq;
 using FluentAssertions;
 using Newtonsoft.Json;
 using Xunit;
 
-namespace System.Linq.Dynamic.Core.Tests
+namespace System.Linq.Dynamic.Core.Tests;
+
+public class DynamicClassTest
 {
-    public class DynamicClassTest
+    [Fact]
+    public void DynamicClass_GetProperties_Should_Work()
     {
-        [Fact]
-        public void DynamicClass_GetProperties_Should_Work()
+        // Arrange
+        var range = new List<object>
         {
-            // Arrange
-            var range = new List<object>
-            {
-                new { FieldName = "TestFieldName", Value = 3.14159 }
-            };
+            new { FieldName = "TestFieldName", Value = 3.14159 }
+        };
 
-            // Act
-            var rangeResult = range.AsQueryable().Select("new(FieldName as FieldName)").ToDynamicList();
-            var item = rangeResult.First();
+        // Act
+        var rangeResult = range.AsQueryable().Select("new(FieldName as FieldName)").ToDynamicList();
+        var item = rangeResult.First();
 
-            var call = () => item.GetDynamicMemberNames();
-            call.Should().NotThrow();
-        }
+        var call = () => item.GetDynamicMemberNames();
+        call.Should().NotThrow();
+    }
 
-        [Fact]
-        public void DynamicClass_GetPropertyValue_Should_Work()
+    [Fact]
+    public void DynamicClass_GetPropertyValue_Should_Work()
+    {
+        // Arrange
+        var test = "Test";
+        var range = new List<object>
         {
-            // Arrange
-            var test = "Test";
-            var range = new List<object>
-            {
-                new { FieldName = test, Value = 3.14159 }
-            };
+            new { FieldName = test, Value = 3.14159 }
+        };
 
-            // Act
-            var rangeResult = range.AsQueryable().Select("new(FieldName as FieldName)").ToDynamicList();
-            var item = rangeResult.First();
+        // Act
+        var rangeResult = range.AsQueryable().Select("new(FieldName as FieldName)").ToDynamicList();
+        var item = rangeResult.First();
 
-            var value = item.FieldName as string;
-            value.Should().Be(test);
-        }
+        var value = item.FieldName as string;
+        value.Should().Be(test);
+    }
 
-        [Fact]
-        public void DynamicClass_GettingValue_ByIndex_Should_Work()
+    [Fact]
+    public void DynamicClass_GettingValue_ByIndex_Should_Work()
+    {
+        // Arrange
+        var test = "Test";
+        var range = new List<object>
         {
-            // Arrange
-            var test = "Test";
-            var range = new List<object>
-            {
-                new { FieldName = test, Value = 3.14159 }
-            };
+            new { FieldName = test, Value = 3.14159 }
+        };
 
-            // Act
-            var rangeResult = range.AsQueryable().Select("new(FieldName as FieldName)").ToDynamicList();
-            var item = rangeResult.First();
+        // Act
+        var rangeResult = range.AsQueryable().Select("new(FieldName as FieldName)").ToDynamicList();
+        var item = rangeResult.First();
 
-            var value = item["FieldName"] as string;
-            value.Should().Be(test);
-        }
+        var value = item["FieldName"] as string;
+        value.Should().Be(test);
+    }
 
-        [Fact]
-        public void DynamicClass_SettingExistingPropertyValue_ByIndex_Should_Work()
+    [Fact]
+    public void DynamicClass_SettingExistingPropertyValue_ByIndex_Should_Work()
+    {
+        // Arrange
+        var test = "Test";
+        var newTest = "abc";
+        var range = new List<object>
         {
-            // Arrange
-            var test = "Test";
-            var newTest = "abc";
-            var range = new List<object>
-            {
-                new { FieldName = test, Value = 3.14159 }
-            };
+            new { FieldName = test, Value = 3.14159 }
+        };
 
-            // Act
-            var rangeResult = range.AsQueryable().Select("new(FieldName as FieldName)").ToDynamicList();
-            var item = rangeResult.First();
+        // Act
+        var rangeResult = range.AsQueryable().Select("new(FieldName as FieldName)").ToDynamicList();
+        var item = rangeResult.First();
 
-            item["FieldName"] = newTest;
-            var value = item["FieldName"] as string;
-            value.Should().Be(newTest);
-        }
+        item["FieldName"] = newTest;
+        var value = item["FieldName"] as string;
+        value.Should().Be(newTest);
+    }
 
-        [Fact]
-        public void DynamicClass_SettingNewProperty_ByIndex_Should_Work()
+    [Fact]
+    public void DynamicClass_SettingNewProperty_ByIndex_Should_Work()
+    {
+        // Arrange
+        var test = "Test";
+        var newTest = "abc";
+        var range = new List<object>
         {
-            // Arrange
-            var test = "Test";
-            var newTest = "abc";
-            var range = new List<object>
-            {
-                new { FieldName = test, Value = 3.14159 }
-            };
+            new { FieldName = test, Value = 3.14159 }
+        };
 
-            // Act
-            var rangeResult = range.AsQueryable().Select("new(FieldName as FieldName)").ToDynamicList();
-            var item = rangeResult.First();
+        // Act
+        var rangeResult = range.AsQueryable().Select("new(FieldName as FieldName)").ToDynamicList();
+        var item = rangeResult.First();
 
-            item["X"] = newTest;
-            var value = item["X"] as string;
-            value.Should().Be(newTest);
-        }
+        item["X"] = newTest;
+        var value = item["X"] as string;
+        value.Should().Be(newTest);
+    }
 
-        [Fact]
-        public void DynamicClass_SerializeToJson_Should_Work()
+    [Fact]
+    public void DynamicClass_SerializeToNewtonsoftJson_Should_Work()
+    {
+        // Arrange
+        var props = new[]
         {
-            // Arrange
-            var props = new[]
-            {
-                new DynamicProperty("Name", typeof(string)), 
-                new DynamicProperty("Birthday", typeof(DateTime))
-            };
-            var type = DynamicClassFactory.CreateType(props);
+            new DynamicProperty("Name", typeof(string)), 
+            new DynamicProperty("Birthday", typeof(DateTime))
+        };
+        var type = DynamicClassFactory.CreateType(props);
 
-            var dynamicClass = (DynamicClass) Activator.CreateInstance(type);
-            dynamicClass.SetDynamicPropertyValue("Name", "Albert");
-            dynamicClass.SetDynamicPropertyValue("Birthday", new DateTime(1879, 3, 14));
-            
-            // Act
-            var json = JsonConvert.SerializeObject(dynamicClass);
+        var dynamicClass = (DynamicClass) Activator.CreateInstance(type);
+        dynamicClass.SetDynamicPropertyValue("Name", "Albert");
+        dynamicClass.SetDynamicPropertyValue("Birthday", new DateTime(1879, 3, 14));
+        
+        // Act
+        var json = JsonConvert.SerializeObject(dynamicClass);
 
-            // Assert
-            json.Should().Be("{\"Name\":\"Albert\",\"Birthday\":\"1879-03-14T00:00:00\"}");
-        }
+        // Assert
+        json.Should().Be("{\"Name\":\"Albert\",\"Birthday\":\"1879-03-14T00:00:00\"}");
     }
 }
