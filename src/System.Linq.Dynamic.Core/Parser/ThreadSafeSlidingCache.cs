3d09d491887e3acbd877be7dde6c1e04dd83d137
Travis Whidden <travis.whidden@econnect.tv>
2024-01-19 13:41:09 -0800

    #764 - Rename T1,T2 to TKey, TValue

--- src/System.Linq.Dynamic.Core/Parser/ThreadSafeSlidingCache.cs
+++ src/System.Linq.Dynamic.Core/Parser/ThreadSafeSlidingCache.cs
@@ -2,23 +2,23 @@
 
 namespace System.Linq.Dynamic.Core.Parser
 {
-    internal class ThreadSafeSlidingCache<T1, T2> where T1 : notnull where T2 : notnull
+    internal class ThreadSafeSlidingCache<TKey, TValue> where TKey : notnull where TValue : notnull
     {
-        private readonly ConcurrentDictionary<T1, (T2 Value, DateTime ExpirationTime)> _cache;
+        private readonly ConcurrentDictionary<TKey, (TValue Value, DateTime ExpirationTime)> _cache;
         private readonly TimeSpan _timeToLive;
         private readonly TimeSpan _cleanupFrequency;
         private DateTime _lastCleanupTime = DateTime.MinValue;
 
         public ThreadSafeSlidingCache(TimeSpan timeToLive, TimeSpan? cleanupFrequency = null)
         {
-            _cache = new ConcurrentDictionary<T1, (T2, DateTime)>();
+            _cache = new ConcurrentDictionary<TKey, (TValue, DateTime)>();
             _timeToLive = timeToLive;
             _cleanupFrequency = cleanupFrequency ?? TimeSpan.FromSeconds(10);
         }
 
         public TimeSpan TimeToLive => _timeToLive;
 
-        public void AddOrUpdate(T1 key, T2 value)
+        public void AddOrUpdate(TKey key, TValue value)
         {
             if (key == null) throw new ArgumentNullException(nameof(key));
             if (value == null) throw new ArgumentNullException(nameof(value));
@@ -29,7 +29,7 @@ namespace System.Linq.Dynamic.Core.Parser
             CleanupIfNeeded();
         }
 
-        public bool TryGetValue(T1 key, out T2 value)
+        public bool TryGetValue(TKey key, out TValue value)
         {
             if (key == null) throw new ArgumentNullException(nameof(key));
 
@@ -54,7 +54,7 @@ namespace System.Linq.Dynamic.Core.Parser
             return false;
         }
 
-        public bool Remove(T1 key)
+        public bool Remove(TKey key)
         {
             if (key == null) throw new ArgumentNullException(nameof(key));
             var removed = _cache.TryRemove(key, out _);
