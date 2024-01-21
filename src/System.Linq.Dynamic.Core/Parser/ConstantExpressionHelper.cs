72a6906cf6b6a9e252fb3d6e848a5b6722b6c30e
Travis Whidden <travis.whidden@econnect.tv>
2024-01-21 10:18:53 -0800

    #764 - Dropped Preprocessor Directive for TTL in ConstantExpressionHelper

--- src/System.Linq.Dynamic.Core/Parser/ConstantExpressionHelper.cs
+++ src/System.Linq.Dynamic.Core/Parser/ConstantExpressionHelper.cs
@@ -5,11 +5,7 @@ namespace System.Linq.Dynamic.Core.Parser
 {
     internal static class ConstantExpressionHelper
     {
-#if DEBUG
-        private static readonly TimeSpan TimeToLivePeriod = TimeSpan.FromSeconds(10);
-#else
         private static readonly TimeSpan TimeToLivePeriod = TimeSpan.FromMinutes(10);
-#endif
 
         public static readonly ThreadSafeSlidingCache<object, Expression> Expressions = new(TimeToLivePeriod);
         private static readonly ThreadSafeSlidingCache<Expression, string> Literals = new(TimeToLivePeriod);
