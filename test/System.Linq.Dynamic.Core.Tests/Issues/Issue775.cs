b43be0866067546a15d94d26faba964e26d6d9f4
Stef Heyenrath <stef.heyenrath@gmail.com>
2024-02-10 12:34:24 +0100

    InvalidOperationException

--- test/System.Linq.Dynamic.Core.Tests/Issues/Issue775.cs
+++ test/System.Linq.Dynamic.Core.Tests/Issues/Issue775.cs
@@ -1,5 +1,6 @@
 ﻿using NFluent;
 using System.Linq.Dynamic.Core.Tests.Helpers.Models;
+using FluentAssertions;
 using Xunit;
 
 namespace System.Linq.Dynamic.Core.Tests;
@@ -33,5 +34,19 @@ public partial class QueryableTests
 
         // Assert
         Check.That(result.ToDynamicArray().Cast<Guid>()).ContainsExactly(realResult);
+    }
+
+    [Fact]
+    public void Issue775_Exception()
+    {
+        // Arrange
+        var users = User.GenerateSampleModels(10);
+        var pets = new[] { new Pet() }.AsQueryable();
+
+        // Act
+        Action act = () => users.AsQueryable().Where("Income == @0.Select(Id).XXX()", pets);
+
+        // Assert
+        act.Should().Throw<InvalidOperationException>();
     }
 }
