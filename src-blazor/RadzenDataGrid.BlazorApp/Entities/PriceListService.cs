c7286907c96e16dafc7b0116db6d82708f9cdba3
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2024-02-14 20:50:36 +0100

    Add RadzenDataGrid.BlazorApp example (#780)
    * Add RadzenDataGrid.BlazorApp example
    
    * use real db
    
    * nuget
    
    * ---

---
+++ src-blazor/RadzenDataGrid.BlazorApp/Entities/PriceListService.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp1.Entities;
+
+public class PriceListService
+{
+	[Key]
+	public int Id { get; set; }
+
+	// [Required]
+	public PriceListServiceBase ServiceBase { get; set; } = new();
+}
---
+++ src-blazor/RadzenDataGrid.BlazorApp/Entities/PriceListServiceBase.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorApp1.Entities;
+
+public class PriceListServiceBase
+{
+	public int Index { get; set; }
+
+	public string? Code { get; set; }
+
+	public string? ServiceName { get; set; }
+}
