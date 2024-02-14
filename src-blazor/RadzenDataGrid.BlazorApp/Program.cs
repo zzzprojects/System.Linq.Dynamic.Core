b6ac968b2f7c800022bc4857c8d6fcb81dd921f5
Stef Heyenrath <stef.heyenrath@gmail.com>
2024-02-14 14:21:18 +0100

    Add RadzenDataGrid.BlazorApp example

---
+++ src-blazor/RadzenDataGrid.BlazorApp/Program.cs
@@ -0,0 +1,39 @@
+using BlazorApp1.Components;
+using Radzen;
+
+namespace BlazorApp1;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddRazorComponents()
+            .AddInteractiveServerComponents();
+
+        builder.Services.AddRadzenComponents();
+
+        builder.Services.AddDbContext<MyDbContext>();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseStaticFiles();
+        app.UseAntiforgery();
+
+        app.MapRazorComponents<App>()
+            .AddInteractiveServerRenderMode();
+
+        app.Run();}
+}
