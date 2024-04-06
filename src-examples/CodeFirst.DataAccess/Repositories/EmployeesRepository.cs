f8eddd61cf5026bdd57b0de66694e124437b1bf0
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2024-04-06 21:23:37 +0200

    Add CodeFirst examples for .NET 6 and 8 (#785)

---
+++ src-examples/CodeFirst.DataAccess/Repositories/EmployeesRepository.cs
@@ -0,0 +1,11 @@
+﻿using CodeFirst.DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeFirst.DataAccess.Repositories;
+
+public class EmployeesRepository : BaseRepository<Employees>
+{
+    public EmployeesRepository(DbContext baseContext) : base(baseContext)
+    {
+    }
+}
