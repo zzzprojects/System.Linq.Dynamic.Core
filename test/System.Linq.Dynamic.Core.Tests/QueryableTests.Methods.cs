7c4f39af89e1bf8bed6cfbdd5c5703120edee003
Stef Heyenrath <stef.heyenrath@gmail.com>
2024-04-17 17:13:56 +0200

    wip

--- test/System.Linq.Dynamic.Core.Tests/QueryableTests.Methods.cs
+++ test/System.Linq.Dynamic.Core.Tests/QueryableTests.Methods.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq.Dynamic.Core.CustomTypeProviders;
+using System.Linq.Expressions;
+using FluentAssertions;
 using Moq;
 using Xunit;
 
@@ -7,6 +9,39 @@ namespace System.Linq.Dynamic.Core.Tests;
 
 public partial class QueryableTests
 {
+    [DynamicLinqType]
+    public class QueryTest
+    {
+        public int Foo { get; set; }
+    }
+
+    [DynamicLinqType]
+    public class QueryStart
+    {
+        public IMyQuery<QueryTest> Start { get; set; } = null!;
+    }
+
+    [DynamicLinqType]
+    public interface IMyQuery<T>
+    {
+        IMyQuery<TNew> Select<TNew>(Expression<Func<T, TNew>> exp);
+    }
+
+    [Fact]
+    public void CallMethod_With_ExpressionAsArgument()
+    {
+        // Arrange
+        var query = new[] { new QueryStart() }.AsQueryable();
+
+        var real = query.Select(q => q.Start.Select(s => s.Foo));
+
+        // Act
+        var result = query.Select<Foo>("Start.Select(q => q.Foo)");
+
+        // Assert
+        result.Should().NotBeNull();
+    }
+
     [Fact]
     public void CallMethod()
     {
