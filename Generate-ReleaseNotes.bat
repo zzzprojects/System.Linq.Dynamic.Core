b183afcdf23adaffa4941bc4d83de105953e2d15
Stef Heyenrath <stef.heyenrath@gmail.com>
2024-04-17 18:44:08 +0200

    v1.3.11

--- Generate-ReleaseNotes.bat
+++ Generate-ReleaseNotes.bat
@@ -1,5 +1,5 @@
 rem https://github.com/StefH/GitHubReleaseNotes
 
-SET version=v1.3.10
+SET version=v1.3.11
 
 GitHubReleaseNotes --output CHANGELOG.md --exclude-labels invalid question documentation wontfix --language en --version %version% --token %GH_TOKEN%
