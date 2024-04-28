a4b4d0a72d3922983ac79c4157e62a895d36c578
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2024-04-28 20:58:32 +0200

    Support implicit boolean operator for logical operations (And, Or) (#806)

--- src/System.Linq.Dynamic.Core/Tokenizer/TokenId.cs
+++ src/System.Linq.Dynamic.Core/Tokenizer/TokenId.cs
@@ -1,49 +1,125 @@
 ﻿#pragma warning disable CS1591
-namespace System.Linq.Dynamic.Core.Tokenizer
+namespace System.Linq.Dynamic.Core.Tokenizer;
+
+/// <summary>
+/// TokenId which defines the text which is parsed.
+/// </summary>
+public enum TokenId
 {
-    /// <summary>
-    /// TokenId which defines the text which is parsed.
-    /// </summary>
-    public enum TokenId
-    {
-        Unknown,
-        End,
-        Identifier,
-        StringLiteral,
-        IntegerLiteral,
-        RealLiteral,
-        Exclamation,
-        Percent,
-        Ampersand,
-        OpenParen,
-        CloseParen,
-        OpenCurlyParen,
-        CloseCurlyParen,
-        Asterisk,
-        Plus,
-        Comma,
-        Minus,
-        Dot,
-        Slash,
-        Colon,
-        LessThan,
-        Equal,
-        GreaterThan,
-        Question,
-        OpenBracket,
-        CloseBracket,
-        Bar,
-        ExclamationEqual,
-        DoubleAmpersand,
-        LessThanEqual,
-        LessGreater,
-        DoubleEqual,
-        GreaterThanEqual,
-        DoubleBar,
-        DoubleGreaterThan,
-        DoubleLessThan,
-        NullCoalescing,
-        Lambda,
-        NullPropagation
-    }
+    Unknown,
+
+
+    End,
+
+
+    Identifier,
+
+
+    StringLiteral,
+
+
+    IntegerLiteral,
+
+
+    RealLiteral,
+
+    // !
+    Exclamation,
+
+    // % (Modulus Operator)
+    Percent,
+
+    // &
+    Ampersand,
+
+    // (
+    OpenParen,
+
+    // )
+    CloseParen,
+
+    // {
+    OpenCurlyParen,
+
+    // }
+    CloseCurlyParen,
+
+    // *
+    Asterisk,
+
+    // +
+    Plus,
+
+    // ,
+    Comma,
+
+    // -
+    Minus,
+
+    // .
+    Dot,
+
+    // /
+    Slash,
+
+    // :
+    Colon,
+
+    // <
+    LessThan,
+
+    // =
+    Equal,
+
+    // >
+    GreaterThan,
+
+    // ?
+    Question,
+
+    // [
+    OpenBracket,
+
+    // ]
+    CloseBracket,
+
+    // |
+    Bar,
+
+    // !=
+    ExclamationEqual,
+
+    // &&
+    DoubleAmpersand,
+
+    // <=
+    LessThanEqual,
+
+    // <>
+    LessGreater,
+
+    // ==
+    DoubleEqual,
+
+
+    // >=
+    GreaterThanEqual,
+
+    // ||
+    DoubleBar,
+
+    // >> (Shift Operator)
+    DoubleGreaterThan,
+
+    // << (Shift Operator)
+    DoubleLessThan,
+
+    // ??
+    NullCoalescing,
+
+    // =>
+    Lambda,
+
+    // ?.
+    NullPropagation
 }
