8cbe29585bab244cb10ca1e8efd1cb8d82961c7f
Stef Heyenrath <stef.heyenrath@gmail.com>
2024-06-25 20:30:20 +0200

    v1.4.2

--- test/System.Linq.Dynamic.Core.Tests/Parser/NumberParserTests.cs
+++ test/System.Linq.Dynamic.Core.Tests/Parser/NumberParserTests.cs
@@ -5,200 +5,195 @@ using System.Linq.Dynamic.Core.Parser;
 using System.Linq.Expressions;
 using Xunit;
 
-namespace System.Linq.Dynamic.Core.Tests.Parser
+namespace System.Linq.Dynamic.Core.Tests.Parser;
+
+public class NumberParserTests
 {
-    public class NumberParserTests
+    private readonly ParsingConfig _parsingConfig = new();
+
+    public static object?[][] Decimals()
     {
-        private readonly ParsingConfig _parsingConfig = new ParsingConfig();
+        return
+        [
+            ["de-DE", "1", 1m],
+            ["de-DE", "-42", -42m],
+            ["de-DE", "3,215", 3.215m],
+            ["de-DE", "3.215", 3215m],
 
-        public static object[][] Decimals()
+            [null, "1", 1m],
+            [null, "-42", -42m],
+            [null, "3,215", 3215m],
+            [null, "3.215", 3.215m]
+        ];
+    }
+
+    [Theory]
+    [MemberData(nameof(Decimals))]
+    public void NumberParser_ParseNumber_Decimal(string? culture, string text, decimal expected)
+    {
+        // Arrange
+        if (culture != null)
         {
-            return new object[][]
-            {
-                new object[] {"de-DE", "1", 1m},
-                new object[] {"de-DE", "-42", -42m},
-                new object[] {"de-DE", "3,215", 3.215m},
-                new object[] {"de-DE", "3.215", 3215m},
-
-                new object[] {null, "1", 1m},
-                new object[] {null, "-42", -42m},
-                new object[] {null, "3,215", 3215m},
-                new object[] {null, "3.215", 3.215m}
-            };
+            _parsingConfig.NumberParseCulture = CultureInfo.CreateSpecificCulture(culture);
         }
 
-        [Theory]
-        [MemberData(nameof(Decimals))]
-        public void NumberParser_ParseNumber_Decimal(string culture, string text, decimal expected)
+        // Act
+        var result = new NumberParser(_parsingConfig).ParseNumber(text, typeof(decimal));
+
+        // Assert
+        result.Should().Be(expected);
+    }
+
+    public static IEnumerable<object?[]> Floats()
+    {
+        return new object?[][]
         {
-            // Arrange
-            if (culture != null)
-            {
-                _parsingConfig.NumberParseCulture = CultureInfo.CreateSpecificCulture(culture);
-            }
+            ["de-DE", "1", 1f],
+            ["de-DE", "-42", -42f],
+            ["de-DE", "3,215", 3.215f],
+            ["de-DE", "3.215", 3215f],
+            ["de-DE", "1,2345E-4", 0.00012345f],
+            ["de-DE", "1,2345e-4", 0.00012345f],
+            ["de-DE", "1,2345E4", 12345d],
+            ["de-DE", "1,2345e4", 12345d],
 
-            // Act
-            var result = new NumberParser(_parsingConfig).ParseNumber(text, typeof(decimal));
+            [null, "1", 1f],
+            [null, "-42", -42f],
+            [null, "3,215", 3215f],
+            [null, "3.215", 3.215f],
+            [null, "1.2345E-4", 0.00012345f],
+            [null, "1.2345e-4", 0.00012345f],
+            [null, "1.2345E4", 12345f],
+            [null, "1.2345e4", 12345f]
+        };
+    }
 
-            // Assert
-            result.Should().Be(expected);
+    [Theory]
+    [MemberData(nameof(Floats))]
+    public void NumberParser_ParseNumber_Float(string? culture, string text, float expected)
+    {
+        // Arrange
+        if (culture != null)
+        {
+            _parsingConfig.NumberParseCulture = CultureInfo.CreateSpecificCulture(culture);
         }
 
-        public static object[][] Floats()
-        {
-            return new object[][]
-            {
-                new object[] {"de-DE", "1", 1f},
-                new object[] {"de-DE", "-42", -42f},
-                new object[] {"de-DE", "3,215", 3.215f},
-                new object[] {"de-DE", "3.215", 3215f},
-                new object[] {"de-DE", "1,2345E-4", 0.00012345f},
-                new object[] {"de-DE", "1,2345e-4", 0.00012345f},
-                new object[] {"de-DE", "1,2345E4", 12345d},
-                new object[] {"de-DE", "1,2345e4", 12345d},
+        // Act
+        var result = new NumberParser(_parsingConfig).ParseNumber(text, typeof(float));
 
-                new object[] {null, "1", 1f},
-                new object[] {null, "-42", -42f},
-                new object[] {null, "3,215", 3215f},
-                new object[] {null, "3.215", 3.215f},
-                new object[] {null, "1.2345E-4", 0.00012345f},
-                new object[] {null, "1.2345e-4", 0.00012345f},
-                new object[] {null, "1.2345E4", 12345f},
-                new object[] {null, "1.2345e4", 12345f}
-            };
+        // Assert
+        result.Should().Be(expected);
+    }
+
+    public static IEnumerable<object?[]> Doubles()
+    {
+        return new object?[][]
+        {
+            ["de-DE", "1", 1d],
+            ["de-DE", "-42", -42d],
+            ["de-DE", "3,215", 3.215d],
+            ["de-DE", "3.215", 3215d],
+            ["de-DE", "1,2345E-4", 0.00012345d],
+            ["de-DE", "1,2345e-4", 0.00012345d],
+            ["de-DE", "1,2345E4", 12345d],
+            ["de-DE", "1,2345e4", 12345d],
+
+            [null, "1", 1d],
+            [null, "-42", -42d],
+            [null, "3,215", 3215d],
+            [null, "3.215", 3.215d],
+            [null, "1.2345E-4", 0.00012345d],
+            [null, "1.2345e-4", 0.00012345d],
+            [null, "1.2345E4", 12345d],
+            [null, "1.2345e4", 12345d]
+        };
+    }
+
+    [Theory]
+    [MemberData(nameof(Doubles))]
+    public void NumberParser_ParseNumber_Double(string? culture, string text, double expected)
+    {
+        // Arrange
+        if (culture != null)
+        {
+            _parsingConfig.NumberParseCulture = CultureInfo.CreateSpecificCulture(culture);
         }
 
-        [Theory]
-        [MemberData(nameof(Floats))]
-        public void NumberParser_ParseNumber_Float(string culture, string text, float expected)
-        {
-            // Arrange
-            if (culture != null)
-            {
-                _parsingConfig.NumberParseCulture = CultureInfo.CreateSpecificCulture(culture);
-            }
+        // Act
+        var result = new NumberParser(_parsingConfig).ParseNumber(text, typeof(double));
 
-            // Act
-            var result = new NumberParser(_parsingConfig).ParseNumber(text, typeof(float));
+        // Assert
+        result.Should().Be(expected);
+    }
 
-            // Assert
-            result.Should().Be(expected);
-        }
+    [Theory]
+    [InlineData("42", 42)]
+    [InlineData("-42", -42)]
+    [InlineData("77u", 77)]
+    [InlineData("77l", 77)]
+    [InlineData("77ul", 77)]
+    [InlineData("0xff", 255)]
+    [InlineData("-0xff", -255)]
+    [InlineData("0b1100000011101", 6173)]
+    [InlineData("-0b1100000011101", -6173)]
+    [InlineData("123d", 123d)]
+    [InlineData("123f", 123f)]
+    [InlineData("123m", 123)]
+    [InlineData("-123d", -123d)]
+    [InlineData("-123f", -123f)]
+    [InlineData("-123m", -123)]
+    public void NumberParser_ParseIntegerLiteral(string text, double expected)
+    {
+        // Act
+        var result = new NumberParser(_parsingConfig).ParseIntegerLiteral(0, text) as ConstantExpression;
 
-        public static IEnumerable<object[]> Doubles()
-        {
-            return new object[][]
-            {
-                new object[] {"de-DE", "1", 1d},
-                new object[] {"de-DE", "-42", -42d},
-                new object[] {"de-DE", "3,215", 3.215d},
-                new object[] {"de-DE", "3.215", 3215d},
-                new object[] {"de-DE", "1,2345E-4", 0.00012345d},
-                new object[] {"de-DE", "1,2345e-4", 0.00012345d},
-                new object[] {"de-DE", "1,2345E4", 12345d},
-                new object[] {"de-DE", "1,2345e4", 12345d},
+        // Assert
+        result?.Value.Should().Be(expected);
+    }
 
-                new object[] {null, "1", 1d},
-                new object[] {null, "-42", -42d},
-                new object[] {null, "3,215", 3215d},
-                new object[] {null, "3.215", 3.215d},
-                new object[] {null, "1.2345E-4", 0.00012345d},
-                new object[] {null, "1.2345e-4", 0.00012345d},
-                new object[] {null, "1.2345E4", 12345d},
-                new object[] {null, "1.2345e4", 12345d}
-            };
-        }
+    [Theory]
+    [InlineData("42", 'm', 42)]
+    [InlineData("-42", 'm', -42)]
+    [InlineData("42m", 'm', 42)]
+    [InlineData("-42m", 'm', -42)]
+    public void NumberParser_ParseDecimalLiteral(string text, char qualifier, decimal expected)
+    {
+        // Act
+        var result = new NumberParser(_parsingConfig).ParseRealLiteral(text, qualifier, true) as ConstantExpression;
 
-        [Theory]
-        [MemberData(nameof(Doubles))]
-        public void NumberParser_ParseNumber_Double(string culture, string text, double expected)
-        {
-            // Arrange
-            if (culture != null)
-            {
-                _parsingConfig.NumberParseCulture = CultureInfo.CreateSpecificCulture(culture);
-            }
+        // Assert
+        result?.Value.Should().Be(expected);
+    }
 
-            // Act
-            var result = new NumberParser(_parsingConfig).ParseNumber(text, typeof(double));
+    [Theory]
+    [InlineData("42", 'd', 42)]
+    [InlineData("-42", 'd', -42)]
+    [InlineData("42d", 'd', 42)]
+    [InlineData("-42d", 'd', -42)]
+    public void NumberParser_ParseDoubleLiteral(string text, char qualifier, double expected)
+    {
+        // Arrange
 
-            // Assert
-            result.Should().Be(expected);
-        }
+        // Act
+        var result = new NumberParser(_parsingConfig).ParseRealLiteral(text, qualifier, true) as ConstantExpression;
 
-        [Theory]
-        [InlineData("42", 42)]
-        [InlineData("-42", -42)]
-        [InlineData("77u", 77)]
-        [InlineData("77l", 77)]
-        [InlineData("77ul", 77)]
-        [InlineData("0xff", 255)]
-        [InlineData("-0xff", -255)]
-        [InlineData("0b1100000011101", 6173)]
-        [InlineData("-0b1100000011101", -6173)]
-        [InlineData("123d", 123d)]
-        [InlineData("123f", 123f)]
-        [InlineData("123m", 123)]
-        [InlineData("-123d", -123d)]
-        [InlineData("-123f", -123f)]
-        [InlineData("-123m", -123)]
-        public void NumberParser_ParseIntegerLiteral(string text, double expected)
-        {
-            // Arrange
+        // Assert
+        result?.Value.Should().Be(expected);
+    }
 
-            // Act
-            var result = new NumberParser(_parsingConfig).ParseIntegerLiteral(0, text) as ConstantExpression;
+    [Theory]
+    [InlineData("42", 'f', 42)]
+    [InlineData("-42", 'f', -42)]
+    [InlineData("42f", 'f', 42)]
+    [InlineData("-42f", 'f', -42)]
+    public void NumberParser_ParseFloatLiteral(string text, char qualifier, float expected)
+    {
+        // Arrange
 
-            // Assert
-            result.Value.Should().Be(expected);
-        }
+        // Act
+        var result = new NumberParser(_parsingConfig).ParseRealLiteral(text, qualifier, true) as ConstantExpression;
 
-        [Theory]
-        [InlineData("42", 'm', 42)]
-        [InlineData("-42", 'm', -42)]
-        [InlineData("42m", 'm', 42)]
-        [InlineData("-42m", 'm', -42)]
-        public void NumberParser_ParseDecimalLiteral(string text, char qualifier, decimal expected)
-        {
-            // Arrange
-
-            // Act
-            var result = new NumberParser(_parsingConfig).ParseRealLiteral(text, qualifier, true) as ConstantExpression;
-
-            // Assert
-            result!.Value.Should().Be(expected);
-        }
-
-        [Theory]
-        [InlineData("42", 'd', 42)]
-        [InlineData("-42", 'd', -42)]
-        [InlineData("42d", 'd', 42)]
-        [InlineData("-42d", 'd', -42)]
-        public void NumberParser_ParseDoubleLiteral(string text, char qualifier, double expected)
-        {
-            // Arrange
-
-            // Act
-            var result = new NumberParser(_parsingConfig).ParseRealLiteral(text, qualifier, true) as ConstantExpression;
-
-            // Assert
-            result!.Value.Should().Be(expected);
-        }
-
-        [Theory]
-        [InlineData("42", 'f', 42)]
-        [InlineData("-42", 'f', -42)]
-        [InlineData("42f", 'f', 42)]
-        [InlineData("-42f", 'f', -42)]
-        public void NumberParser_ParseFloatLiteral(string text, char qualifier, float expected)
-        {
-            // Arrange
-
-            // Act
-            var result = new NumberParser(_parsingConfig).ParseRealLiteral(text, qualifier, true) as ConstantExpression;
-
-            // Assert
-            result!.Value.Should().Be(expected);
-        }
+        // Assert
+        result?.Value.Should().Be(expected);
     }
 }
