23927b39668b150e8b0696834efb39a04876209b
Stef Heyenrath <stef.heyenrath@gmail.com>
2024-08-21 22:54:23 +0200

    fix ca

---
+++ src/System.Linq.Dynamic.Core/Compatibility/CodeAnalysis/DynamicallyAccessedMembersAttribute.cs
@@ -0,0 +1,19 @@
+﻿#if !NET6_0_OR_GREATER
+// ReSharper disable once CheckNamespace
+namespace System.Diagnostics.CodeAnalysis;
+
+//
+// Summary:
+//     Indicates that certain members on a specified System.Type are accessed dynamically,
+//     for example, through System.Reflection.
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Method | AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Interface | AttributeTargets.Parameter | AttributeTargets.ReturnValue | AttributeTargets.GenericParameter, Inherited = false)]
+internal sealed class DynamicallyAccessedMembersAttribute : Attribute
+{
+    public DynamicallyAccessedMembersAttribute(DynamicallyAccessedMemberTypes memberTypes)
+    {
+        MemberTypes = memberTypes;
+    }
+
+    public DynamicallyAccessedMemberTypes MemberTypes { get; }
+}
+#endif
