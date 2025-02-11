1fa3e15de79fd2b0fd961c3c2580be8b205a343d
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2025-02-11 17:59:15 +0100

    Fix AbstractDynamicLinqCustomTypeProvider.ResolveTypeBySimpleName to use AdditionalTypes (#896)

---
+++ test/System.Linq.Dynamic.Core.Tests/TestClasses/TestClassWithDynamicLinqAttribute.cs
@@ -0,0 +1,8 @@
+﻿using System.Linq.Dynamic.Core.CustomTypeProviders;
+
+namespace System.Linq.Dynamic.Core.Tests.TestClasses;
+
+[DynamicLinqType]
+internal class TestClassWithDynamicLinqAttribute
+{
+}
