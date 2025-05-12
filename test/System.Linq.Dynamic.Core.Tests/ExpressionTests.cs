cc4e28b8077faddb141ffe8e30316f82003a2496
Stef Heyenrath <stef.heyenrath@gmail.com>
2025-05-12 09:03:42 +0200

    Fix MethodFinder TryFindAggregateMethod to support array

--- test/System.Linq.Dynamic.Core.Tests/ExpressionTests.cs
+++ test/System.Linq.Dynamic.Core.Tests/ExpressionTests.cs
@@ -2155,57 +2155,6 @@ namespace System.Linq.Dynamic.Core.Tests
         }
 
         [Fact]
-        public void ExpressionTests_Sum()
-        {
-            // Arrange
-            int[] initValues = { 1, 2, 3, 4, 5 };
-            var qry = initValues.AsQueryable().Select(x => new { strValue = "str", intValue = x }).GroupBy(x => x.strValue);
-
-            // Act
-            var result = qry.Select("Sum(intValue)").AsDynamicEnumerable().ToArray()[0];
-
-            // Assert
-            Assert.Equal(15, result);
-        }
-
-        [Fact]
-        public void ExpressionTests_Sum_LowerCase()
-        {
-            // Arrange
-            int[] initValues = { 1, 2, 3, 4, 5 };
-            var qry = initValues.AsQueryable().Select(x => new { strValue = "str", intValue = x }).GroupBy(x => x.strValue);
-
-            // Act
-            var result = qry.Select("sum(intValue)").AsDynamicEnumerable().ToArray()[0];
-
-            // Assert
-            Assert.Equal(15, result);
-        }
-
-        [Fact]
-        public void ExpressionTests_Sum2()
-        {
-            // Arrange
-            var initValues = new[]
-            {
-                new SimpleValuesModel { FloatValue = 1 },
-                new SimpleValuesModel { FloatValue = 2 },
-                new SimpleValuesModel { FloatValue = 3 },
-            };
-
-            var qry = initValues.AsQueryable();
-
-            // Act
-            var result = qry.Select("FloatValue").Sum();
-            var result2 = ((IQueryable<float>)qry.Select("FloatValue")).Sum();
-
-            // Assert
-            Assert.Equal(6.0f, result);
-            Assert.Equal(6.0f, result2);
-        }
-
-
-        [Fact]
         public void ExpressionTests_Type_Integer()
         {
             // Arrange
