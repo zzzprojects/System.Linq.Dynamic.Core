f2e0ec7f306f020d7fd70191e91b37b554812203
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2025-09-27 07:30:50 +0200

    Fix GroupByMany using composite key and normal key (#946)
    * Fix GroupByMany using composite key and normal key
    
    * .
    
    * readme

--- test/System.Linq.Dynamic.Core.Tests/QueryableTests.GroupByMany.cs
+++ test/System.Linq.Dynamic.Core.Tests/QueryableTests.GroupByMany.cs
@@ -1,57 +1,82 @@
 ﻿using System.Collections.Generic;
+using FluentAssertions;
 using NFluent;
 using Xunit;
 
-namespace System.Linq.Dynamic.Core.Tests
+namespace System.Linq.Dynamic.Core.Tests;
+
+public partial class QueryableTests
 {
-    public partial class QueryableTests
+    [Fact]
+    public void GroupByMany_Dynamic_LambdaExpressions()
     {
-        [Fact]
-        public void GroupByMany_Dynamic_LambdaExpressions()
+        var lst = new List<Tuple<int, int, int>>
         {
-            var lst = new List<Tuple<int, int, int>>
-            {
-                new Tuple<int, int, int>(1, 1, 1),
-                new Tuple<int, int, int>(1, 1, 2),
-                new Tuple<int, int, int>(1, 1, 3),
-                new Tuple<int, int, int>(2, 2, 4),
-                new Tuple<int, int, int>(2, 2, 5),
-                new Tuple<int, int, int>(2, 2, 6),
-                new Tuple<int, int, int>(2, 3, 7)
-            };
+            new(1, 1, 1),
+            new(1, 1, 2),
+            new(1, 1, 3),
+            new(2, 2, 4),
+            new(2, 2, 5),
+            new(2, 2, 6),
+            new(2, 3, 7)
+        };
 
-            var sel = lst.AsQueryable().GroupByMany(x => x.Item1, x => x.Item2);
+        var sel = lst.GroupByMany(x => x.Item1, x => x.Item2).ToArray();
 
-            Assert.Equal(2, sel.Count());
-            Assert.Single(sel.First().Subgroups);
-            Assert.Equal(2, sel.Skip(1).First().Subgroups.Count());
-        }
+        Assert.Equal(2, sel.Length);
+        Assert.Single(sel.First().Subgroups);
+        Assert.Equal(2, sel.Skip(1).First().Subgroups.Count());
+    }
 
-        [Fact]
-        public void GroupByMany_Dynamic_StringExpressions()
+    [Fact]
+    public void GroupByMany_Dynamic_StringExpressions()
+    {
+        var lst = new List<Tuple<int, int, int>>
         {
-            var lst = new List<Tuple<int, int, int>>
-            {
-                new Tuple<int, int, int>(1, 1, 1),
-                new Tuple<int, int, int>(1, 1, 2),
-                new Tuple<int, int, int>(1, 1, 3),
-                new Tuple<int, int, int>(2, 2, 4),
-                new Tuple<int, int, int>(2, 2, 5),
-                new Tuple<int, int, int>(2, 2, 6),
-                new Tuple<int, int, int>(2, 3, 7)
-            };
+            new(1, 1, 1),
+            new(1, 1, 2),
+            new(1, 1, 3),
+            new(2, 2, 4),
+            new(2, 2, 5),
+            new(2, 2, 6),
+            new(2, 3, 7)
+        };
 
-            var sel = lst.AsQueryable().GroupByMany("Item1", "Item2").ToList();
+        var sel = lst.GroupByMany("Item1", "Item2").ToList();
 
-            Check.That(sel.Count).Equals(2);
+        Check.That(sel.Count).Equals(2);
 
-            var firstGroupResult = sel.First();
-            Check.That(firstGroupResult.ToString()).Equals("1 (3)");
-            Check.That(firstGroupResult.Subgroups.Count()).Equals(1);
+        var firstGroupResult = sel.First();
+        Check.That(firstGroupResult.ToString()).Equals("1 (3)");
+        Check.That(firstGroupResult.Subgroups.Count()).Equals(1);
 
-            var skippedGroupResult = sel.Skip(1).First();
-            Check.That(skippedGroupResult.ToString()).Equals("2 (4)");
-            Check.That(skippedGroupResult.Subgroups.Count()).Equals(2);
-        }
+        var skippedGroupResult = sel.Skip(1).First();
+        Check.That(skippedGroupResult.ToString()).Equals("2 (4)");
+        Check.That(skippedGroupResult.Subgroups.Count()).Equals(2);
+    }
+
+    [Fact]
+    public void GroupByMany_Dynamic_CompositeKey()
+    {
+        // Arrange
+        var data = new[]
+        {
+            new { MachineId = 1, Machine = new { Id = 1, Name = "A" } },
+            new { MachineId = 1, Machine = new { Id = 1, Name = "A" } },
+            new { MachineId = 2, Machine = new { Id = 2, Name = "B" } }
+        };
+
+        // Act
+        var normalResult = data
+            .GroupByMany(d => new { d.MachineId, d.Machine.Name }, a => a.Machine.Id)
+            .Select(x => x.ToString())
+            .ToList();
+        var result = data
+            .GroupByMany("new (MachineId, Machine.Name)", "Machine.Id")
+            .Select(x => x.ToString())
+            .ToList();
+
+        // Assert
+        result.Should().BeEquivalentTo(normalResult);
     }
 }
