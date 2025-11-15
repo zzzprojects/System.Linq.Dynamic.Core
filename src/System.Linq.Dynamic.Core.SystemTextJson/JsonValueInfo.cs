e70bc42055d51bff30934ca5d3cabd9a09f8d552
Stef Heyenrath <stef.heyenrath@gmail.com>
2025-11-15 10:07:17 +0100

    ns

--- src/System.Linq.Dynamic.Core.SystemTextJson/JsonValueInfo.cs
+++ src/System.Linq.Dynamic.Core.SystemTextJson/JsonValueInfo.cs
@@ -1,5 +1,7 @@
 ﻿using System.Text.Json;
 
+namespace System.Linq.Dynamic.Core.SystemTextJson;
+
 internal readonly struct JsonValueInfo(JsonValueKind type, object? value)
 {
     public JsonValueKind Type { get; } = type;
